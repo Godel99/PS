a484885c1fdd011fa483b9493e8f3020df78613a
Godel99 <tylor9632@gmail.com>
2025-12-17 14:39:20 +0900

    [Gold III] Title: 두 배열의 합, Time: 464 ms, Memory: 117460 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -5,29 +5,26 @@ input=sys.stdin.readline
 
 def main():
     T = int(input())
-    A_n = int(input())
+    n = int(input())
     A = list(map(int, input().split()))
-    B_n = int(input())
+    m = int(input())
     B = list(map(int, input().split()))
 
     A_case = defaultdict(int)
 
-    for i in range(A_n):
-        sum_case = A[i]
-        A_case[sum_case] += 1
-        for j in range(i+1, A_n):
-            A_case[sum_case+A[j]] += 1
-            sum_case += A[j]
+    for i in range(n):
+        s = 0
+        for j in range(i, n):
+            A_case[s+A[j]] += 1
+            s += A[j]
 
     total_case = 0
 
-    for i in range(B_n):
-        b_sum = B[i]
-        total_case += A_case[T-b_sum]
-        for j in range(i+1, B_n):
-            b_sum += B[j]
-            total_case += A_case[T-b_sum]
-
+    for i in range(m):
+        s = 0
+        for j in range(i, m):
+            s += B[j]
+            total_case += A_case[T-s]
 
     sys.stdout.write(str(total_case))
 
