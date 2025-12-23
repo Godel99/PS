29efda90349741a2ae0040c5db8e6457d0f7bd3f
Godel99 <tylor9632@gmail.com>
2025-12-23 20:11:36 +0900

    [level 3] Title: 징검다리 건너기, Time: 284.57 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,22 +1,22 @@
 def solution(stones, k):
-    left = 1
-    right = max(stones)
-    
+    left, right = 1, max(stones)
+
     while left <= right:
-        x_cnt = 0
         mid = (left + right) // 2
+        broken = 0
 
         for stone in stones:
-            if x_cnt == k:
-                break
-            if stone <= mid:
-                x_cnt += 1
+            if stone < mid:
+                broken += 1
+                if broken == k:
+                    break
             else:
-                x_cnt = 0
+                broken = 0
 
-        if x_cnt == k:
+        if broken == k:
             right = mid - 1
         else:
-            left = mid + 1
-    
-    return left
+            answer = mid
+            left =  mid + 1
+
+    return answer
