233883359ad3bd8a9b25b7f0e94b0606d95274e4
Godel99 <tylor9632@gmail.com>
2025-12-29 13:50:42 +0900

    [Bronze V] Title: 개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.py
@@ -0,0 +1,13 @@
+import sys
+
+input=sys.stdin.readline
+
+def main():
+    sys.stdout.write('''|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"`    |
+||_/=\\\\__|''')
+
+if __name__ == '__main__':
+    main()
