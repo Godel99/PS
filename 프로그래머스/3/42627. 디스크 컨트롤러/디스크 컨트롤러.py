29db55cfbbf37f4a4e9e8efb3af712a4242e3eee
Godel99 <tylor9632@gmail.com>
2025-12-30 23:20:13 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.79 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,24 +1,46 @@
 import heapq
 
-def solution(jobs):
-    pq = []
+def solution(jobs): 
+    jobs.sort()
     n = len(jobs)
-    jobs.sort(key=lambda x: x[0])
 
-    cur_time = cnt = 0
-    total_time = 0
-    i = 0
-    while i < n:
-        while cnt < n and jobs[cnt][0] <= cur_time:
-            heapq.heappush(pq, (jobs[cnt][1], jobs[cnt][0], cnt))
-            cnt += 1
+    cur_time = idx = total_time = 0
+    pq = []
+
+    while idx < n or pq:
+        while idx < n and jobs[idx][0] <= cur_time:
+            start, duration = jobs[idx]
+            heapq.heappush(pq, (duration, start))
+            idx += 1
 
         if pq:
-            d, s, _ = heapq.heappop(pq)
-            cur_time += d
-            total_time += (cur_time - s)
-            i += 1
+            duration, start = heapq.heappop(pq)
+            cur_time += duration
+            total_time += (cur_time - start)
         else:
-            cur_time = jobs[cnt][0]
+            cur_time = jobs[idx][0]
 
-    return (total_time // n)
+    return total_time // n
+
+# def solution(jobs):
+#     pq = []
+#     n = len(jobs)
+#     jobs.sort(key=lambda x: x[0])
+
+#     cur_time = cnt = 0
+#     total_time = 0
+#     i = 0
+#     while i < n:
+#         while cnt < n and jobs[cnt][0] <= cur_time:
+#             heapq.heappush(pq, (jobs[cnt][1], jobs[cnt][0], cnt))
+#             cnt += 1
+
+#         if pq:
+#             d, s, _ = heapq.heappop(pq)
+#             cur_time += d
+#             total_time += (cur_time - s)
+#             i += 1
+#         else:
+#             cur_time = jobs[cnt][0]
+
+#     return (total_time // n)
