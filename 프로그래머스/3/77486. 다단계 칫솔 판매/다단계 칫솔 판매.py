e1f057151b7083ab55f71b2ed855be2033423aa9
Godel99 <tylor9632@gmail.com>
2025-12-30 13:53:56 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 108.72 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -3,13 +3,12 @@ def solution(enroll, referral, seller, amount):
 
     parent = dict()
 
-    for i, v in enumerate(enroll):
-        parent[v] = referral[i]
+    for e, r in zip(enroll, referral):
+        parent[e] = r
 
- 
-    for i, v in enumerate(seller):
-        cur = v
-        profit = amount[i] * 100
+    for s, a in zip(seller, amount):
+        cur = s
+        profit = a * 100
 
         while cur != '-' and profit > 0:
             give = profit // 10
@@ -18,3 +17,25 @@ def solution(enroll, referral, seller, amount):
             profit = give
 
     return list(result.values())
+
+
+# def solution(enroll, referral, seller, amount):
+#     result = dict.fromkeys(enroll, 0)
+
+#     parent = dict()
+
+#     for i, v in enumerate(enroll):
+#         parent[v] = referral[i]
+
+ 
+#     for i, v in enumerate(seller):
+#         cur = v
+#         profit = amount[i] * 100
+
+#         while cur != '-' and profit > 0:
+#             give = profit // 10
+#             result[cur] += profit - give
+#             cur = parent[cur]
+#             profit = give
+
+#     return list(result.values())
