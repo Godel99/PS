4868a12afe8c744339f95054782049d8b9cdb2c8
Godel99 <tylor9632@gmail.com>
2025-12-31 11:05:58 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -0,0 +1,16 @@
+import sys
+
+input=sys.stdin.readline
+
+def main():
+    n = int(input())
+
+    for i in range(n):
+        for j in range(n-i-1):
+           sys.stdout.write(' ')
+        for k in range(i+1):
+            sys.stdout.write('*')
+        sys.stdout.write('\n')
+ 
+if __name__ == '__main__':
+    main()
