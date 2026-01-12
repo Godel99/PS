31887f3897c4063ee3bb9cbdd91eb473dc8737e1
Godel99 <tylor9632@gmail.com>
2026-01-12 09:37:29 +0900

    [Platinum III] Title: 도형, Time: 40 ms, Memory: 6728 KB -BaekjoonHub

--- 백준/Platinum/1121. 도형/도형.cc
+++ 백준/Platinum/1121. 도형/도형.cc
@@ -2,26 +2,26 @@
 using namespace std;
 using ll = long long;
 
-int a[50];
-ll dp[11][501010];
-
 int main(){
 	cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
 	int n; cin >> n;
-	dp[0][0] = 1;
+	vector<int> a(n);
 	for(int i = 0; i < n; i++) cin >> a[i];
-	sort(a, a+n);
+	sort(a.begin(), a.end());
 	int k; cin >> k;
+	int maxl = a[n-1]+1;
+	vector<vector<ll>> dp(k+1, vector<ll>(maxl+1, 0));
+	dp[0][0] = 1;
 	for(int i = 0; i < n; i++){
-		for(int j = 50000*(k-1); j >= 0; j--){
+		for(int j = maxl; j >= 0; j--){
 			if(j <= a[i]) break;
-			dp[k][500010] += dp[k-1][j];
+			dp[k][maxl] += dp[k-1][j];
 		}
 		for(int l = k-2; l >= 0; l--){
-			for(int j = 50000*l; j >= 0; j--){
-				dp[l+1][j+a[i]] += dp[l][j];
+			for(int j = maxl; j >= 0; j--){
+				dp[l+1][min(maxl, j+a[i])] += dp[l][j];
 			}
 		}
 	}
-	cout << dp[k][500010];
+	cout << dp[k][maxl];
 }
