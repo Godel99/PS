9c080dadb49c98d2d80c2ded0d792665fbc1625e
Godel99 <tylor9632@gmail.com>
2026-01-17 11:47:34 +0900

    [Silver II] Title: 가위 가위 가위, Time: 32 ms, Memory: 13676 KB -BaekjoonHub

--- 백준/Silver/30506. 가위 가위 가위/가위 가위 가위.py
+++ 백준/Silver/30506. 가위 가위 가위/가위 가위 가위.py
@@ -6,18 +6,17 @@ def input():
 
 def main():
     k = int(input())
-    sys.stdout.flush()
     ans = ['0'] * 100
-    s = ['2'] * 100
+    hands = ['2'] * 100
     for i in range(100):
-        s[i] = '0'
-        print("?", ''.join(s))
+        hands[i] = '0'
+        print('?', ''.join(hands))
         sys.stdout.flush()
+        hands[i] = '2'
         newk = int(input())
         if k < newk: ans[i] = '2'
         elif k == newk: ans[i] = '0'
         else: ans[i] = '5'
-        s[i] = '2'
     print('!', ''.join(ans))
     sys.stdout.flush()
 if __name__ == '__main__':
