a4b28d9dde6a47803fbb561c6065af4b6f9b83e5
Godel99 <tylor9632@gmail.com>
2026-01-18 11:48:12 +0900

    [Platinum I] Title: 마지막 요세푸스 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/1179. 마지막 요세푸스 문제/마지막 요세푸스 문제.cc
+++ 백준/Platinum/1179. 마지막 요세푸스 문제/마지막 요세푸스 문제.cc
@@ -2,9 +2,10 @@
 using namespace std;
 using ll = long long;
 
+ll ans;
+
 ll josephus(ll n, ll k){
-    if(n == 1) return 0;
-    if(n < k) return (josephus(n-1, k)+k)%n;
+	if(n < k) return ans;
     ll l = josephus(n-n/k, k) - n%k;
     if(l < 0) return l+n;
     return l+l/(k-1);
@@ -12,5 +13,8 @@ ll josephus(ll n, ll k){
 
 int main(){
     cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
-    ll n, k; cin >> n >> k; if(k == 1) cout << n; else cout << josephus(n, k)+1;
+    ll n, k; cin >> n >> k;
+	for(int i = 1; i < k; i++) ans = (ans+k)%i;
+    if(k == 1) cout << n; else cout << josephus(n, k)+1;
+	return 0;
 }
