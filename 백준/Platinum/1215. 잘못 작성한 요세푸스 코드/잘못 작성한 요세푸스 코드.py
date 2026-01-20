f9245165b060821167c90f7128b39a24131c5bc1
Godel99 <tylor9632@gmail.com>
2026-01-20 21:29:49 +0900

    [Platinum IV] Title: 잘못 작성한 요세푸스 코드, Time: 56 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Platinum/1215. 잘못 작성한 요세푸스 코드/잘못 작성한 요세푸스 코드.py
+++ 백준/Platinum/1215. 잘못 작성한 요세푸스 코드/잘못 작성한 요세푸스 코드.py
@@ -1,33 +1,21 @@
-n, k = map(int, input().split())
+import sys
+def print(*args, sep=" ", end="\n"):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+def input():
+    return sys.stdin.readline().rstrip()
 
-result = 0
+from math import sqrt
 
-# k보다 큰 i에 대해서는 k mod i = k
-if n > k:
-    result += (n - k) * k
-    n = k
-
-# i = 1부터 n까지 k mod i의 합 계산
-# 같은 몫을 가지는 구간을 묶어서 처리
-i = 1
-while i <= n:
-    q = k // i  # k를 i로 나눈 몫
-    r = k % i   # k를 i로 나눈 나머지
-    
-    # 같은 몫 q를 가지는 최대 i 찾기
-    # k // next_i == q를 만족하는 최대 next_i
-    if q == 0:
-        next_i = n
-    else:
-        next_i = min(k // q, n)
-    
-    # i부터 next_i까지의 구간에서 k mod j의 합
-    # k mod j = k - q * j 이므로
-    # 구간 [i, next_i]에서의 합 = k * cnt - q * (i + ... + next_i)
-    cnt = next_i - i + 1
-    sum_range = (i + next_i) * cnt // 2
-    result += k * cnt - q * sum_range
-    
-    i = next_i + 1
-
-print(result)
+def main():
+    n, k = map(int, input().split())
+    sq = int(sqrt(k)); ans = 0
+    for i in range(1, min(k//sq, n)+1): ans += k%i
+    for i in range(1, sq):
+        l = min(k//i, n); r = k//(i+1)+1
+        if l < r: continue
+        ans += k*(l-r+1)-(l+r)*(l-r+1)*i//2
+    ans += k*max(0, n-k)
+    print(ans)
+    return 0
+if __name__ == '__main__':
+    sys.exit(main())
