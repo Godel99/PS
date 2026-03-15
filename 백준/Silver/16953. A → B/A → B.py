b7b8a01cd1139e022db8cc30196d35441559ec76
Godel99 <tylor9632@gmail.com>
2026-03-15 21:38:49 +0900

    [Silver II] Title: A → B, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -2,19 +2,14 @@ import sys
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
-from collections import deque
-
 def main():
     a, b = map(int, input().split())
-    dq = deque([(a, 1)])
-    while dq:
-        cur, cnt = dq.popleft()
-        if cur == b:
-            print(cnt)
-            return
-        for nxt in [cur<<1, cur*10+1]:
-            if nxt <= b:
-                dq.append((nxt, cnt+1))
-    print(-1)
+    cnt = 1
+    while b > a:
+        if b&1 == 0: b >>= 1
+        elif b%10 == 1: b //= 10
+        else: break
+        cnt += 1
+    print(cnt if b == a else -1)
 if __name__ == '__main__':
     main()
