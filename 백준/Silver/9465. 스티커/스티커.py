bba461ed345d18937909c0d21638a06b0446e1ff
Godel99 <tylor9632@gmail.com>
2026-03-17 14:01:52 +0900

    [Silver I] Title: 스티커, Time: 428 ms, Memory: 48400 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -6,12 +6,15 @@ def main():
     t = int(input())
     for _ in range(t):
         n = int(input())
-        score = [[0]+list(map(int, input().split())) for _ in range(2)]
-        dp = [[0]*(n+1) for _ in range(2)]
-        dp[0][1], dp[1][1] = score[0][1], score[1][1]
-        for i in range(2, n+1):
-            dp[0][i] = max(dp[1][i-1], dp[1][i-2])+score[0][i]
-            dp[1][i] = max(dp[0][i-1], dp[0][i-2])+score[1][i]
-        print(max(dp[0][-1], dp[1][-1]))
+        score = [list(map(int, input().split())) for _ in range(2)]
+        if n == 1:
+            print(max(score[0][0], score[1][0]))
+            continue
+        score[0][1] += score[1][0]
+        score[1][1] += score[0][0]
+        for i in range(2, n):
+            score[0][i] += max(score[1][i-1], score[1][i-2])
+            score[1][i] += max(score[0][i-1], score[0][i-2])
+        print(max(score[0][-1], score[1][-1]))
 if __name__ == '__main__':
     main()
