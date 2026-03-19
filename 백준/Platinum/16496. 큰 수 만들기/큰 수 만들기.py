0c4800794b777fe8d0dd157b6942c29fba50eb27
Godel99 <tylor9632@gmail.com>
2026-03-19 09:46:39 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -2,12 +2,10 @@ import sys
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
-from functools import cmp_to_key
-
 def main():
     n = int(input())
     a = list(input().split())
-    a.sort(key=cmp_to_key(lambda a, b: -1 if a+b > b+a else 1))
+    a.sort(key=lambda x: x*9, reverse=True)
     ans = ''.join(a)
     print('0' if ans[0] == '0' else ans)
 if __name__ == '__main__':
