2abd864a8ab4f497b1b79a123c7b5cf2d0a2d278
Godel99 <tylor9632@gmail.com>
2026-03-19 23:10:34 +0900

    [Platinum IV] Title: Determinant Strikes Back, Time: 636 ms, Memory: 53004 KB -BaekjoonHub

--- 백준/Platinum/20791. Determinant Strikes Back/Determinant Strikes Back.py
+++ 백준/Platinum/20791. Determinant Strikes Back/Determinant Strikes Back.py
@@ -5,16 +5,10 @@ input = lambda: sys.stdin.readline().rstrip('\r\n')
 MOD = 1_000_000_007
 
 def main():
-    data = sys.stdin.read().split()
-    if not data: return
-    idx = 0
-    while idx < len(data):
-        n, x = list(map(int, data[idx:idx+2]))
-        idx += 2
-        a = list(map(int,data[idx:idx+n]))
-        idx += n
-        b = list(map(int,data[idx:idx+n]))
-        idx += n
+    while nx := list(map(int, input().split())):
+        n, x = nx
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split()))
         s = 0
         for i in range(n): s = (s+(a[i]*b[i]))%MOD
         print((pow(x, n, MOD)+(pow(x, n-1, MOD)*s)%MOD)%MOD)
