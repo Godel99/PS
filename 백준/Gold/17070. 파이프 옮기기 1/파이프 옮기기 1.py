7133eb2c9231e423026c7d81b3c0d8fbc93c8daf
Godel99 <tylor9632@gmail.com>
2026-03-20 23:20:27 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -9,7 +9,7 @@ def main():
     dp = [[[0]*3 for _ in range(n)] for _ in range(n)]
     dp[0][1][0] = 1
     for r in range(n):
-        for c in range(n):
+        for c in range(2, n):
             if board[r][c] == 1: continue 
             if c > 0: dp[r][c][0] += dp[r][c-1][0] + dp[r][c-1][2]
             if r > 0: dp[r][c][1] += dp[r-1][c][1] + dp[r-1][c][2]
