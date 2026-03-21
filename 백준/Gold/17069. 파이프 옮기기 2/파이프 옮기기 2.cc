94f36b41206d4066fccfa292c867de73ba5be891
Godel99 <tylor9632@gmail.com>
2026-03-21 10:36:10 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.cc
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.cc
@@ -12,7 +12,7 @@ int main(){
     vector<vector<al3>> dp(n, vector<al3>(n, {0, 0, 0}));
     dp[0][1][0] = 1;
     for(int i = 0; i < n; i++) for(int j = 2; j < n; j++){
-        if(board[i][j] == 1) continue;
+        if(board[i][j]) continue;
         if(j > 0) dp[i][j][0] += dp[i][j-1][0]+dp[i][j-1][2];
         if(i > 0) dp[i][j][1] += dp[i-1][j][1]+dp[i-1][j][2];
         if(i > 0 && j > 0 && !board[i-1][j] && !board[i][j-1]) dp[i][j][2] += dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
