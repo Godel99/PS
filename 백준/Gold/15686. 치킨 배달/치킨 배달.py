b09b2b77986688eb893c290fb1ab7e06693c5770
Godel99 <tylor9632@gmail.com>
2026-03-21 14:15:56 +0900

    [Gold V] Title: 치킨 배달, Time: 112 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -2,7 +2,6 @@ import sys
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
-from itertools import combinations
 from math import inf
 
 def main():
@@ -15,16 +14,25 @@ def main():
             elif row[c] == 2: chicken.append((r, c))
     h_len, c_len = len(house), len(chicken)
     dist = [[abs(hr-cr)+abs(hc-cc) for cr, cc in chicken] for hr, hc in house]
-    mindist = inf
-    for com in combinations(range(c_len), m):
-        curdist = 0
-        for h_i in range(h_len):
-            minhdist = inf
-            for c_i in com: minhdist = min(minhdist, dist[h_i][c_i])
-            curdist += minhdist
-            if curdist >= mindist: break
-        else: mindist = min(curdist, mindist)
-    print(mindist)
+    ans = inf
+    selected = []
+    def dfs(idx):
+        nonlocal ans
+        if len(selected) == m:
+            curdist = 0
+            for h_i in range(h_len):
+                minhdist = inf
+                for c_i in selected: minhdist = min(minhdist, dist[h_i][c_i])
+                curdist += minhdist
+                if curdist >= ans: return
+            ans = curdist
+            return
+        for i in range(idx, c_len): 
+            selected.append(i)
+            dfs(i+1)
+            selected.pop()
+    dfs(0)
+    print(ans)        
     return
 if __name__ == '__main__':
     main()
