64539b70ef39cde9f0446c2f76680066d977493e
Godel99 <tylor9632@gmail.com>
2026-03-21 22:04:35 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 88 ms, Memory: 37864 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -7,17 +7,21 @@ from collections import deque
 def main():
     n, k = map(int, input().split())
     if n == k: print(0); return
-    maxl = (n if n > k else k)+2
-    dp = [float('inf')]*maxl
+    maxl = max(n, k)+2
+    dp = [-1]*maxl
     dp[n] = 0
     dq = deque([n])
     while dq:
         cur = dq.popleft()
-        for nxt, sec in [(cur-1, 1), (cur+1, 1), (cur*2, 0)]:
-            if 0 <= nxt < maxl and dp[nxt] > dp[cur]+sec:
-                dp[nxt] = dp[cur]+sec   
-                if sec: dq.append(nxt)
-                else: dq.appendleft(nxt)
+        if cur == k: break
+        nxt = cur*2
+        if 0 <= nxt < maxl and dp[nxt] == -1:
+            dp[nxt] = dp[cur]
+            dq.appendleft(nxt)
+        for nxt in [cur-1, cur+1]:
+            if 0 <= nxt < maxl and dp[nxt] == -1:
+                dp[nxt] = dp[cur]+1
+                dq.append(nxt)
     print(dp[k])
     return
 if __name__ == '__main__':
