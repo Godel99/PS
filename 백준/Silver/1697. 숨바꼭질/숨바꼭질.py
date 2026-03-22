d661b3061c732a3c30a4e47a01351c8becb31d8a
Godel99 <tylor9632@gmail.com>
2026-03-22 12:21:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,21 +1,16 @@
-import sys
+import sys 
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
-from collections import deque
+def f(n, k):
+    if n >= k: return n - k
+    if k == 1: return abs(n - 1)
+    if k&1: return -~min(f(n, k + 1), f(n, k - 1))
+    else: return min(k - n, f(n, k // 2)+1)
 
 def main():
     n, k = map(int, input().split())
-    if n == k: print(0); return
-    maxl = (n if n > k else k)+2
-    dp = [0]*maxl
-    dq = deque([n])
-    while dq:
-        cur = dq.popleft()
-        for nxt in [cur-1, cur+1, cur*2]:
-            if 0 <= nxt < maxl and not dp[nxt]:
-                dp[nxt] = dp[cur]+1
-                dq.append(nxt)
-    print(dp[k])
+    print(f(n, k))    
+    return
 if __name__ == '__main__':
     main()
