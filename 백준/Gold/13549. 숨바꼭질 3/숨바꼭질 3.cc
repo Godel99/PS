3091e64c06752ed9815f6463f80d0e6aaeac7599
Godel99 <tylor9632@gmail.com>
2026-03-22 14:51:14 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -2,29 +2,16 @@
 using namespace std;
 using ll = long long;
 
+int f(int n, int k){
+    if(n >= k) return n-k;
+    if(k == 1) return abs(n-1);
+    if(k&1) return -~min(f(n, k-1), f(n, k+1));
+    else return min(k-n, f(n, k>>1));
+}
+
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
     int n, k; cin >> n >> k;
-    if(n ==  k) return !(cout << 0);
-    int maxl = max(n, k)+2;
-    vector<int> dp(maxl, -1);
-    dp[n] = 0;
-    deque<int> dq; dq.push_back(n);
-    while(dq.size()){
-        int cur = dq.front(); dq.pop_front();
-        if(cur == k) break;
-        int nxt = cur*2;
-        if(0 <= nxt && nxt < maxl && dp[nxt] == -1){
-            dp[nxt] = dp[cur];
-            dq.push_front(nxt);
-        }
-        for(auto nxt: {cur-1, cur+1}){
-             if(0 <= nxt && nxt < maxl && dp[nxt] == -1){
-                dp[nxt] = dp[cur]+1;
-                dq.push_back(nxt);
-             }
-        }
-    }
-    cout << dp[k];
+    cout << f(n, k);
     return 0;
 } 
