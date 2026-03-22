a348ae74c5aa88a02100b596112d2d20698805dc
Godel99 <tylor9632@gmail.com>
2026-03-22 14:02:53 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,12 +1,14 @@
-import sys
+import sys 
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+input = lambda: sys.stdin.readline().rstrip('\r\n')
 
-def f(n, k):
-    if n >= k: return n - k
-    if k == 1: return abs(n - 1)
-    
-    if k % 2:
-        return 1 + min(f(n, k + 1), f(n, k - 1))
-    else:
-        return min(k - n, f(n, k // 2))
-n, m = map(int, sys.stdin.readline().split())
-print(f(n, m))
+def main():
+    n, k = map(int, input().split())
+    def f(n, k):
+        if n >= k: return n-k
+        if k == 1: return abs(n-1)
+        if k&1: return -~min(f(n, k-1), f(n, k+1))
+        else: return min(k-n, f(n, k//2))
+    print(f(n, k))
+if __name__ == '__main__':
+    main()
