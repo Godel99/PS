d1527b65802e3d368ba177f33f72a4b9e955d1f0
Godel99 <tylor9632@gmail.com>
2026-03-22 21:49:19 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 76 ms, Memory: 12692 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,65 +1,35 @@
-#include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#include<bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using ai3 = array<int, 3>;
 
-#define X first
-#define Y second
+int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
 
-const int INF = int(1e9);
-
-string board[1000];
-int dist1[1000][1000];
-int dist2[1000][1000];
-int n, m;
-
-int main() {
-	fastio;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> board[i];
-
-	queue<pair<int, int>> Q;
-	Q.push({ 0, 0 });
-	dist1[0][0] = 1;
-	while (!Q.empty()) {
-		auto cur = Q.front(); Q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = cur.X + "2101"[i] - '1';
-			int ny = cur.Y + "1012"[i] - '1';
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (board[nx][ny] == '1' || dist1[nx][ny] != 0) continue;
-			Q.push({ nx, ny });
-			dist1[nx][ny] = dist1[cur.X][cur.Y] + 1;
-		}
-	}
-
-	Q.push({ n - 1, m - 1 });
-	dist2[n - 1][m - 1] = 1;
-	while (!Q.empty()) {
-		auto cur = Q.front(); Q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = cur.X + "2101"[i] - '1';
-			int ny = cur.Y + "1012"[i] - '1';
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (board[nx][ny] == '1' || dist2[nx][ny] != 0) continue;
-			Q.push({ nx, ny });
-			dist2[nx][ny] = dist2[cur.X][cur.Y] + 1;
-		}
-	}
-
-	int mx = INF;
-	if (dist1[n - 1][m - 1] != 0) mx = dist1[n - 1][m - 1];
-	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
-		if (board[i][j] == '0') continue;
-		int mx1 = INF, mx2 = INF;
-		for (int k = 0; k < 4; k++) {
-			int nx = i + "2101"[k] - '1';
-			int ny = j + "1012"[k] - '1';
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (dist1[nx][ny] > 0) mx1 = min(mx1, dist1[nx][ny]);
-			if (dist2[nx][ny] > 0) mx2 = min(mx2, dist2[nx][ny]);
-		}
-		mx = min(mx, mx1 + mx2 + 1);
-	}
-	if (mx != INF) cout << mx << '\n';
-	else cout << -1 << '\n';
+int main(){
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    int n, m; cin >> n >> m;
+    vector<vector<char>> board(n, vector<char>(m));
+    for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) cin >> board[i][j];
+    vector<vector<array<int,2>>> dp(n, vector<array<int,2>>(m, {0, 0}));
+    dp[0][0][0] = 1;
+    deque<ai3> dq;
+    dq.push_back({0, 0, 0});
+    while(!dq.empty()){
+        auto[x, y, b] = dq.front(); dq.pop_front();
+        if(x == n-1 && y == m-1) return !(cout << dp[x][y][b]);
+        for(int dir = 0; dir < 4; dir++){
+            int nx = x+dx[dir], ny = y+dy[dir];
+            if (nx < 0 or nx >= n or ny < 0 or ny >= m) continue;
+            if(board[nx][ny] == '0' && dp[nx][ny][b] == 0){
+                dp[nx][ny][b] = dp[x][y][b]+1;
+                dq.push_back({nx, ny, b});
+            }
+            if(board[nx][ny] == '1' && b == 0 && dp[nx][ny][1] == 0){
+                dp[nx][ny][1] = dp[x][y][b]+1;
+                dq.push_back({nx, ny, 1});
+            }
+        }
+    }
+    cout << -1;
+    return 0;
 }
