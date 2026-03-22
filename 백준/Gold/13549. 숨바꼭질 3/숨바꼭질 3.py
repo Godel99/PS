8dcef04c44136f0245dd64108374c622855c678a
Godel99 <tylor9632@gmail.com>
2026-03-22 12:03:33 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -4,11 +4,9 @@ def f(n, k):
     if n >= k: return n - k
     if k == 1: return abs(n - 1)
     
-    if k % 2 == 0:
-        return min(k - n, f(n, k // 2))
-    else:
+    if k % 2:
         return 1 + min(f(n, k + 1), f(n, k - 1))
-
-# 실행부
+    else:
+        return min(k - n, f(n, k // 2))
 n, m = map(int, sys.stdin.readline().split())
 print(f(n, m))
