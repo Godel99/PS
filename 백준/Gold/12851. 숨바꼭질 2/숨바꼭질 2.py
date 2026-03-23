982befb1622cd06ed493b915cd2c673c386b9ae1
Godel99 <tylor9632@gmail.com>
2026-03-23 14:38:00 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -2,27 +2,23 @@ import sys
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
-from collections import deque
-
 def main():
     n, k = map(int, input().split())
-    if n == k: print(0); print(1); return
-    maxl = max(n, k)+2
-    dp = [-1]*maxl
-    dp[n] = 0
-    dq = deque([n])
-    cnt = 0
-    while dq:
-        cur = dq.popleft()
-        if cur == k: 
-            cnt += 1
-            continue
-        for nxt in [cur-1, cur+1, cur*2]:
-            if 0 <= nxt < maxl and (dp[nxt] == -1 or dp[nxt] == dp[cur]+1):
-                dp[nxt] = dp[cur]+1
-                dq.append(nxt)
-    print(dp[k])
-    print(cnt)
+    def f(n, k):
+        if n >= k: return n-k, 1
+        if k == 1: return 1, 1
+        if k&1:
+            a, b = f(n, k-1)
+            c, d = f(n, k+1)
+            if a == c: return-~a, b+d
+            elif a < c: return-~a, b
+            else: return -~c, d
+        else:
+            a, b = f(n, k>>1)
+            if k-n == a+1: return k-n, -~b
+            elif k-n < a+1: return k-n, 1
+            else: return -~a, b
+    print(*f(n, k))
     return
 if __name__ == '__main__':
     main()
