a9ed9d2dd10147fa08b1a26d1ab0e3047dd332a3
Godel99 <tylor9632@gmail.com>
2026-03-23 14:46:33 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,29 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using pii = pair<int, int>;
+
+pii f(int n, int k){
+    if(n >= k) return {n-k, 1};
+    if(k == 1) return {1, 1};
+    if(k&1){
+        auto [a, b] = f(n, ~-k);
+        auto [c, d] = f(n, -~k);
+        if(a == c) return {-~a, b+d};
+        else if(a < c) return {-~a, b};
+        else return {-~c, d};
+    }
+    else{
+        auto [a, b] = f(n, k>>1);
+        if(k-n == a+1) return {k-n, -~b};
+        else if(k-n < a+1) return {k-n, 1};
+        else return {-~a, b};
+    }
+}
 
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
     int n, k; cin >> n >> k;
-    if(n ==  k) return !(cout << 0 << '\n' << 1);
-    int maxl = max(n, k)+2;
-    vector<int> dp(maxl, -1);
-    dp[n] = 0;
-    queue<int> q; q.push(n);
-    int cnt = 0;
-    while(q.size()){
-        int cur = q.front(); q.pop();
-        if(cur == k){
-            cnt++;
-            continue;
-        }
-        for(auto nxt: {cur-1, cur+1, cur*2}){
-             if(0 <= nxt && nxt < maxl && (dp[nxt] == -1 || dp[nxt] == dp[cur]+1)){
-                dp[nxt] = dp[cur]+1;
-                q.push(nxt);
-             }
-        }
-    }
-    cout << dp[k] << '\n' << cnt;
+    auto [t, c] = f(n, k);
+    cout << t << '\n' << c;
     return 0;
 } 
