6e337370889c0a26baa42b5d9f2bf6828f17c442
Godel99 <tylor9632@gmail.com>
2026-03-24 22:17:57 +0900

    [Silver I] Title: 악보는 거들 뿐, Time: 252 ms, Memory: 88880 KB -BaekjoonHub

--- 백준/Silver/27972. 악보는 거들 뿐/악보는 거들 뿐.py
+++ 백준/Silver/27972. 악보는 거들 뿐/악보는 거들 뿐.py
@@ -8,11 +8,10 @@ def main():
     pre = p[0]
     pcnt = ncnt = ans = 1
     for i in range(1, m):
-        if pre == p[i]: continue
-        if pre > p[i]: ncnt += 1; pcnt = 1
+        if p[i-1] == p[i]: continue
+        if p[i-1] > p[i]: ncnt += 1; pcnt = 1
         else: pcnt += 1; ncnt = 1
-        ans = max(ans, pcnt, ncnt)
-        pre = p[i]
+        ans = max(ans, pcnt, ncnt)       
     print(ans)
     return
 if __name__ == '__main__':
