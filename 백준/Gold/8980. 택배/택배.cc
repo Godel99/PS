aed9f17bade106ce46e1967ac7371c8b9c974030
Godel99 <tylor9632@gmail.com>
2026-03-24 21:29:37 +0900

    [Gold II] Title: 택배, Time: 4 ms, Memory: 2264 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.cc
+++ 백준/Gold/8980. 택배/택배.cc
@@ -3,6 +3,50 @@ using namespace std;
 using ll = long long;
 using aiii = array<int, 3>;
 
+struct SegTree {
+    int n;
+    vector<int> tree, lazy;
+    SegTree(int n) : n(n) {
+        tree.assign(4*n, 0);
+        lazy.assign(4*n, 0);
+    }
+
+    void push(int node) {
+        if (lazy[node] != 0) {
+            for (int nxt : {node*2, node*2+1}) {
+                tree[nxt] += lazy[node];
+                lazy[nxt] += lazy[node];
+            }
+            lazy[node] = 0;
+        }
+    }
+
+    void range_add(int node, int l, int r, int ql, int qr, int val) {
+        if (qr < l || r < ql) return;
+        if (ql <= l && r <= qr) {
+            tree[node] += val;
+            lazy[node] += val;
+            return;
+        }
+        push(node);
+        int mid = (l + r) / 2;
+        range_add(node*2, l, mid, ql, qr, val);
+        range_add(node*2+1, mid+1, r, ql, qr, val);
+        tree[node] = max(tree[node*2], tree[node*2+1]);
+    }
+
+    int query_max(int node, int l, int r, int ql, int qr) {
+        if (qr < l || r < ql) return 0;
+        if (ql <= l && r <= qr) return tree[node];
+        push(node);
+        int mid = (l + r) / 2;
+        return max(
+            query_max(node*2, l, mid, ql, qr),
+            query_max(node*2+1, mid+1, r, ql, qr)
+        );
+    }
+};
+
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
     int n, c, m; cin >> n >> c >> m;
@@ -11,13 +55,12 @@ int main(){
     sort(d.begin(), d.end(), [](const aiii& a, const aiii& b){
         return a[1] < b[1];
     });
-    vector<int> cap(n+1, 0);
+    SegTree seg(n);
     int ans = 0;
     for(auto &[s, e, w]: d){
-        int mx = 0;
-        for(int i = s; i < e; i++) mx = max(mx, cap[i]);
+        int mx = seg.query_max(1, 1, n, s, e-1);
         int can = min(w, c-mx);
-        for(int i = s; i < e; i++) cap[i] += can;
+        seg.range_add(1, 1, n, s, e-1, can);
         ans += can;
     }
     cout << ans;
