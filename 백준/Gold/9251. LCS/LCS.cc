5b37021024859576bd460b5c0c1b5420c7a27b81
Godel99 <tylor9632@gmail.com>
2026-03-27 21:11:05 +0900

    [Gold IV] Title: LCS, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    string a, b; cin >> a >> b;
+    int n = a.size(), m = b.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    for(int i = 1; i <= n; i++) for(int j = 1; j <= m; j++){
+        if(a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1]+1;
+        else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+    }
+    cout << dp[n][m];
+    return 0;
+} 
