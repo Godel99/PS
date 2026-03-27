222a41b9b248964010664ad9b3bbf3f386d23ee8
Godel99 <tylor9632@gmail.com>
2026-03-27 10:54:09 +0900

    [Platinum IV] Title: Cowpatibility, Time: 1000 ms, Memory: 95576 KB -BaekjoonHub

--- 백준/Platinum/16764. Cowpatibility/Cowpatibility.py
+++ 백준/Platinum/16764. Cowpatibility/Cowpatibility.py
@@ -2,24 +2,21 @@ import sys
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
+from collections import Counter
+from itertools import combinations
+
 def main():
     n = int(input())
-    v = []
+    v = Counter()
     for _ in range(n):
         a = sorted(map(int, input().split()))
-        for j in range(1, 32):
-            tmp = []
-            for k in range(5):
-                if j>>k & 1: tmp.append(a[k])
-            v.append(tuple(tmp))
-    v.sort()
-    ans = n*(n-1)//2; cnt = 1
-    for i in range(1, len(v)):
-        if v[i] == v[i-1]: cnt += 1
-        else:
-            ans += (-1 if len(v[i-1])&1 else 1)*cnt*(cnt-1)//2
-            cnt = 1
-    ans += (-1 if len(v[-1])&1 else 1)*cnt*(cnt-1)//2
+        for r in range(1, 6):
+            for comb in combinations(a, r):
+                v[comb] += 1
+    ans = n*(n-1)//2
+    for comb, cnt in v.items():
+        if cnt < 2: continue
+        ans += (-1 if len(comb)&1 else 1)*cnt*(cnt-1)//2
     print(ans)
     return
 if __name__ == '__main__':
