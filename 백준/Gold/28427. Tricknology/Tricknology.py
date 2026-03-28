a076a8f7473d18e6815b5ad54de6eb1cfb032a04
Godel99 <tylor9632@gmail.com>
2026-03-28 15:10:31 +0900

    [Gold III] Title: Tricknology, Time: 648 ms, Memory: 85744 KB -BaekjoonHub

--- 백준/Gold/28427. Tricknology/Tricknology.py
+++ 백준/Gold/28427. Tricknology/Tricknology.py
@@ -6,20 +6,18 @@ from math import sqrt
 
 def main():
     maxR, maxX = 1000001, 500000
-    isp = [1]*(maxR+1)
-    isp[0] = isp[1] = 0
+    p = [1]*(maxR+1); p[0] = p[1] = 0
     for i in range(2, int(sqrt(maxR))+1):
-        if isp[i]:
-            for j in range(i*i, maxR+1, i): isp[j] = 0
-    pres = [0]*(maxX+1)
+        if p[i]: p[i*i : maxR+1 : i] = [0] * len(range(i*i, maxR+1, i))
+    ps = [0]*(maxX+1)
     for x in range(1, maxX+1):
-        if isp[2*x+1]: pres[x] = pres[x-1]+1
-        else: pres[x] = pres[x-1]
+        if p[2*x+1]: ps[x] = ps[x-1]+1
+        else: ps[x] = ps[x-1]
     q = int(input())
     ans = []
     for _ in range(q):
         L, R = map(int, input().split())
-        ans.append(str(pres[R-1]-pres[L-1]))
+        ans.append(str(ps[R-1]-ps[L-1]))
     print('\n'.join(ans))
     return
 if __name__ == '__main__':
