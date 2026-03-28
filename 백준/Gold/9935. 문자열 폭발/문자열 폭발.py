0eebfe35081302b086ebff83f64573e1084663e2
Godel99 <tylor9632@gmail.com>
2026-03-28 11:15:00 +0900

    [Gold IV] Title: 문자열 폭발, Time: 152 ms, Memory: 44572 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -3,14 +3,13 @@ print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
 def main():
-    s, t = input(), input()
-    st = []
+    s, t = input(), list(input())
+    n, lc = len(t), t[-1]
+    ans = []
     for c in s:
-        st.append(c)
-        if c == t[-1] and len(st) >= len(t):
-            if ''.join(st[-len(t):]) == t:
-                for _ in range(len(t)): st.pop()
-    print(''.join(st) if st else 'FRULA')
+        ans.append(c)
+        if c == lc and ans[-n:] == t: del ans[-n:]
+    print(''.join(ans) if ans else 'FRULA')
     return
 if __name__ == '__main__':
     main()
