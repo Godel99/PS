6ed132206be7a868c2d2e292245a86c31003fc4f
Godel99 <tylor9632@gmail.com>
2026-03-28 23:08:48 +0900

    [Gold III] Title: Tricknology, Time: 868 ms, Memory: 83616 KB -BaekjoonHub

--- 백준/Gold/28427. Tricknology/Tricknology.py
+++ 백준/Gold/28427. Tricknology/Tricknology.py
@@ -2,13 +2,13 @@ import sys
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
-from math import sqrt
-
 def main():
-    maxR, maxX = 1000001, 500000
+    maxR, maxX = 1_000_001, 500_000
     p = [1]*(maxR+1); p[0] = p[1] = 0
-    for i in range(2, int(sqrt(maxR))+1):
-        if p[i]: p[i*i : maxR+1 : i] = [0] * len(range(i*i, maxR+1, i))
+    idx = 2
+    while idx < maxR:
+        if p[idx]: p[idx*idx:maxR+1:idx] = [0]*len(range(idx*idx, maxR+1, idx))
+        idx += 1
     ps = [0]*(maxX+1)
     for x in range(1, maxX+1):
         if p[2*x+1]: ps[x] = ps[x-1]+1
