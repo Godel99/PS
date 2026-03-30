0198fbd9b6026120c6cabb1d619f3a8c002b0569
Godel99 <tylor9632@gmail.com>
2026-03-31 07:37:00 +0900

    [Platinum IV] Title: James Ferraro - Live at Primavera Sound 2012, Time: 56 ms, Memory: 37208 KB -BaekjoonHub

--- 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.py
+++ 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.py
@@ -6,20 +6,18 @@ def main():
     N = int(input())
     isp = bytearray([1])*(N+1)
     isp[0] = isp[1] = 0
-    for i in range(2, int((N+1)**0.5)+1):
-        if isp[i]: isp[i*i:(N+1):i] = bytearray([0])*len(range(i*i, (N+1), i))
+    isp[4::2] = bytearray([0]) * len(range(4, N + 1, 2))
+    for i in range(3, int((N+1)**0.5)+1, 2):
+        if isp[i]: isp[i*i:(N+1):i*2] = bytearray([0])*len(range(i*i, (N+1), i*2))
     ans = []
     while N > 7:
-        flag = 0
-        for LL in range(N-1, 0, -1):
-            if (LL+N)%3 == 0 and (LL+N)&1:
-                for L in range(LL, 0, -6):
-                    if isp[(L+N)//3]:
-                        for i in range((N-L+1)//2): ans.append(f'{L+i} {N-i}')
-                        N = L-1
-                        flag = 1
-                        break
-            if flag: break
+        r = ((3-N)%6+6)%6
+        s = (N-1)-((N-1-r)%6+6)%6
+        for L in range(s, 0, -6):
+            if isp[(L+N)//3]:
+                for i in range((N-L+1)//2): ans.append(f'{L+i} {N-i}')
+                N = L-1
+                break
     if N >= 6: ans.extend(('1 5', '2 4', '3 6'))
     elif N >= 4: ans.extend(('1 3', '2 4'))
     elif N == 3: ans.append('1 3')
