ecc592ba5b284af707c9da8a88f4fe6201314122
Godel99 <tylor9632@gmail.com>
2026-03-30 20:47:18 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -2,19 +2,28 @@
 using namespace std;
 using ll = long long;
 
+vector<int> LIS(const vector<int>& a){
+    int n = a.size();
+    vector<int> lst, ret(n, 0);
+    for(int i = 0; i < n; i++){
+        auto it = lower_bound(lst.begin(), lst.end(), a[i]);
+        if(it == lst.end()) lst.push_back(a[i]);
+        else *it = a[i];
+        ret[i] = lst.size();
+    }
+    return ret;
+}
+
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
     int n; cin >> n;
     vector<int> a(n);
     for(int i = 0; i < n; i++) cin >> a[i];
-    vector<int> up(n, 1), down(n, 1);
-    for(int i = 0; i < n; i++) for(int j = 0; j < i; j++){
-        if(a[j] < a[i]) up[i] = max(up[i], up[j]+1);
-    }
-    for(int i = n-1; i >= 0; i--) for(int j = i+1; j < n; j++){
-        if(a[j] < a[i]) down[i] = max(down[i], down[j]+1);
-    }
-    int ans = 1;
+    vector<int> up = LIS(a);
+    reverse(a.begin(), a.end());
+    vector<int> down = LIS(a);
+    reverse(down.begin(), down.end());
+    int ans = 0;
     for(int i = 0; i < n; i++) ans = max(ans, up[i]+down[i]-1);
     cout << ans;
     return 0;
