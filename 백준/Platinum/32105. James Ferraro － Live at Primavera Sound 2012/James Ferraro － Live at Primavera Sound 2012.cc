c755236101015573b6e4a819939f1f17ff052706
Godel99 <tylor9632@gmail.com>
2026-03-31 07:50:20 +0900

    [Platinum IV] Title: James Ferraro - Live at Primavera Sound 2012, Time: 8 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.cc
+++ 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.cc
@@ -8,26 +8,25 @@ int main(){
     int N; cin >> N;
     bool isp[N+1]; memset(isp, 1, sizeof(isp));
     isp[0] = isp[1] = 0;
-    for(int i = 2; i*i <= N+1; i++){
+    for(int i = 4; i <= N; i += 2) isp[i] = 0;
+    for(int i = 3; i*i <= N+1; i++){
         if(isp[i]) for(int j = i*i; j <= N+1; j+=i) isp[j] = 0;
     }
     vector<pll> ans; ans.reserve(N/2);
     while(N > 7){
-        bool flag = 0;
-        for(int LL = N-1; LL > 0; LL--) if((LL+N)%3 == 0 && (LL+N)&1){
-            for(int L = LL; L > 0; L -=6) if(isp[(L+N)/3]){
-                for(int i = 0; i < (N-L+1)/2; i++) ans.push_back({L+i, N-i});
-                N = L-1;
-                flag = 1;
-                break;
-            }
-            if(flag) break;
+        int r = ((3-N)%6+6)%6;
+        int s = (N-1)-((N-1-r)%6+6)%6;
+        for(int L = s; L > 0; L -=6) if(isp[(L+N)/3]){
+            for(int i = 0; i < (N-L+1)/2; i++) ans.push_back({L+i, N-i});
+            N = L-1;
+            break;
         }
+    
     }
-    if(N >= 6) ans.insert(ans.end(), {{1, 5}, {2, 4}, {3, 6}});
-    else if(N >= 4) ans.insert(ans.end(), {{1, 3}, {2, 4}});
+    if(N >= 6) ans.insert(ans.end(), { {1, 5}, {2, 4}, {3, 6} });
+    else if(N >= 4) ans.insert(ans.end(), { {1, 3}, {2, 4} });
     else if(N == 3) ans.push_back({1, 3});
     cout << ans.size() << '\n';
     for(auto& [x, y] : ans) cout << x << ' ' << y << '\n';
     return 0;
-} 
+}
