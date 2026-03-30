49f6f8a230014caa65075c28798d95397e2b762c
Godel99 <tylor9632@gmail.com>
2026-03-30 10:32:56 +0900

    [Platinum IV] Title: James Ferraro - Live at Primavera Sound 2012, Time: 8 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.cc
+++ 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.cc
@@ -5,29 +5,28 @@ using pll = pair<ll, ll>;
 
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    int n, maxV; cin >> n; maxV = n+1;
-    bool isp[maxV]; memset(isp, 1, sizeof(isp));
+    int N; cin >> N;
+    bool isp[N+1]; memset(isp, 1, sizeof(isp));
     isp[0] = isp[1] = 0;
-    for(int i = 2; i*i <= maxV; i++){
-        if(isp[i]) for(int j = i*i; j <= maxV; j+=i) isp[j] = 0;
+    for(int i = 2; i*i <= N+1; i++){
+        if(isp[i]) for(int j = i*i; j <= N+1; j+=i) isp[j] = 0;
     }
-    int R = n;
-    vector<pll> ans; ans.reserve(n/2);
-    while(R > 7){
+    vector<pll> ans; ans.reserve(N/2);
+    while(N > 7){
         bool flag = 0;
-        for(int LL = R-1; LL > 0; LL--) if((LL+R)%3 == 0 && (LL+R)&1){
-            for(int L = LL; L > 0; L -=6) if(isp[(L+R)/3]){
-                for(int i = 0; i < (R-L+1)/2; i++) ans.push_back({L+i, R-i});
-                R = L-1;
+        for(int LL = N-1; LL > 0; LL--) if((LL+N)%3 == 0 && (LL+N)&1){
+            for(int L = LL; L > 0; L -=6) if(isp[(L+N)/3]){
+                for(int i = 0; i < (N-L+1)/2; i++) ans.push_back({L+i, N-i});
+                N = L-1;
                 flag = 1;
                 break;
             }
             if(flag) break;
         }
     }
-    if(R >= 6) ans.insert(ans.end(), {{1, 5}, {2, 4}, {3, 6}});
-    else if(R >= 4) ans.insert(ans.end(), {{1, 3}, {2, 4}});
-    else if(R == 3) ans.push_back({1, 3});
+    if(N >= 6) ans.insert(ans.end(), {{1, 5}, {2, 4}, {3, 6}});
+    else if(N >= 4) ans.insert(ans.end(), {{1, 3}, {2, 4}});
+    else if(N == 3) ans.push_back({1, 3});
     cout << ans.size() << '\n';
     for(auto& [x, y] : ans) cout << x << ' ' << y << '\n';
     return 0;
