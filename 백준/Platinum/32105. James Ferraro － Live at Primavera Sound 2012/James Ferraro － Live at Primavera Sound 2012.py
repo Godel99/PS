5d22e6c36e1da9ee19e32263a09a0996203c3648
Godel99 <tylor9632@gmail.com>
2026-03-30 10:32:29 +0900

    [Platinum IV] Title: James Ferraro - Live at Primavera Sound 2012, Time: 60 ms, Memory: 37208 KB -BaekjoonHub

--- 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.py
+++ 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.py
@@ -3,26 +3,26 @@ print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
 def main():
-    n = int(input())
-    maxV = n+1
-    isp = bytearray([1])*maxV
+    N = int(input())
+    isp = bytearray([1])*(N+1)
     isp[0] = isp[1] = 0
-    for i in range(2, int(maxV**0.5)+1):
-        if isp[i]: isp[i*i:maxV:i] = bytearray([0])*len(range(i*i, maxV, i))
+    for i in range(2, int((N+1)**0.5)+1):
+        if isp[i]: isp[i*i:(N+1):i] = bytearray([0])*len(range(i*i, (N+1), i))
     ans = []
-    R = n
-    while R > 7:
+    while N > 7:
         flag = 0
-        for LL in range(R-1, 0, -1):
-            if (LL+R)%3 == 0 and isp[(LL+R)//3]:
-                for i in range((R-LL+1)//2): ans.append(f'{LL+i} {R-i}')
-                R = LL-1
-                flag = 1
-                break
+        for LL in range(N-1, 0, -1):
+            if (LL+N)%3 == 0 and (LL+N)&1:
+                for L in range(LL, 0, -6):
+                    if isp[(L+N)//3]:
+                        for i in range((N-L+1)//2): ans.append(f'{L+i} {N-i}')
+                        N = L-1
+                        flag = 1
+                        break
             if flag: break
-    if R >= 6: ans.extend(('1 5', '2 4', '3 6'))
-    elif R >= 4: ans.extend(('1 3', '2 4'))
-    elif R == 3: ans.append('1 3')
+    if N >= 6: ans.extend(('1 5', '2 4', '3 6'))
+    elif N >= 4: ans.extend(('1 3', '2 4'))
+    elif N == 3: ans.append('1 3')
     print(f'{len(ans)}\n'+'\n'.join(ans))
     return
 if __name__ == '__main__':
