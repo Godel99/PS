0951ebb32584c83a3211d8375300efd9e37bbd3d
Godel99 <tylor9632@gmail.com>
2026-03-31 10:47:13 +0900

    [Gold III] Title: Tricknology, Time: 136 ms, Memory: 4832 KB -BaekjoonHub

--- 백준/Gold/28427. Tricknology/Tricknology.cc
+++ 백준/Gold/28427. Tricknology/Tricknology.cc
@@ -5,13 +5,15 @@ using ll = long long;
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
     int maxR = 1000001, maxX = 500000;
-    vector<bool> p(maxR+1, 1); p[0] = p[1] = 0;
-    for(int i = 2; i*i <= maxR; i++){
-        if(p[i]) for(int j = i*i; j <= maxR; j += i) p[j] = 0;
+    bool isp[maxR+1]; memset(isp, 1, sizeof(isp));
+    isp[0] = isp[1] = 0;
+    for(int i = 4; i <= maxR; i += 2) isp[i] = 0;
+    for(int i = 3; i*i <= maxR+1; i += 2){
+        if(isp[i]) for(int j = i*i; j <= maxR+1; j+=i*2) isp[j] = 0;
     }
     vector<int> ps(maxX+1, 0);
     for(int x = 1; x <= maxX; x++){
-        if(p[2*x+1]) ps[x] = ps[x-1]+1;
+        if(isp[2*x+1]) ps[x] = ps[x-1]+1;
         else ps[x] = ps[x-1];
     }
     int q; cin >> q;
@@ -20,4 +22,4 @@ int main(){
         cout << ps[R-1]-ps[L-1] << '\n';
     }
     return 0;
-} 
+}
