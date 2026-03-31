008d3ff626c38160c05a7fec0d991354db4d02b5
Godel99 <tylor9632@gmail.com>
2026-03-31 10:41:54 +0900

    [Gold III] Title: Tricknology, Time: 716 ms, Memory: 77280 KB -BaekjoonHub

--- 백준/Gold/28427. Tricknology/Tricknology.py
+++ 백준/Gold/28427. Tricknology/Tricknology.py
@@ -3,15 +3,15 @@ print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
 def main():
-    maxR, maxX = 1_000_001, 500_000
-    p = [1]*(maxR+1); p[0] = p[1] = 0
-    idx = 2
-    while idx < maxR:
-        if p[idx]: p[idx*idx:maxR+1:idx] = [0]*len(range(idx*idx, maxR+1, idx))
-        idx += 1
+    maxR, maxX = 1000001, 500000
+    isp = bytearray([1])*(maxR+1)
+    isp[0] = isp[1] = 0
+    isp[4::2] = bytearray([0]) * len(range(4, maxR + 1, 2))
+    for i in range(3, int((maxR+1)**0.5)+1, 2):
+        if isp[i]: isp[i*i:(maxR+1):i*2] = bytearray([0])*len(range(i*i, (maxR+1), i*2))
     ps = [0]*(maxX+1)
     for x in range(1, maxX+1):
-        if p[2*x+1]: ps[x] = ps[x-1]+1
+        if isp[2*x+1]: ps[x] = ps[x-1]+1
         else: ps[x] = ps[x-1]
     q = int(input())
     ans = []
