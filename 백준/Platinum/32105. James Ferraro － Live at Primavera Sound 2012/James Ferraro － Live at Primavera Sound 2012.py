190caca08b5fbd95b9ec5486c343d888294f6c7d
Godel99 <tylor9632@gmail.com>
2026-03-31 10:44:18 +0900

    [Platinum IV] Title: James Ferraro - Live at Primavera Sound 2012, Time: 68 ms, Memory: 39468 KB -BaekjoonHub

--- 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.py
+++ 백준/Platinum/32105. James Ferraro － Live at Primavera Sound 2012/James Ferraro － Live at Primavera Sound 2012.py
@@ -2,13 +2,14 @@ import sys
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
+from math import sqrt
+
 def main():
     N = int(input())
     isp = bytearray([1])*(N+1)
     isp[0] = isp[1] = 0
-    isp[4::2] = bytearray([0]) * len(range(4, N + 1, 2))
-    for i in range(3, int((N+1)**0.5)+1, 2):
-        if isp[i]: isp[i*i:(N+1):i*2] = bytearray([0])*len(range(i*i, (N+1), i*2))
+    for i in range(2, int(sqrt(N+1))+1):
+        if isp[i]: isp[i*i:(N+1):i] = bytearray([0])*len(range(i*i, (N+1), i))
     ans = []
     while N > 7:
         r = ((3-N)%6+6)%6
