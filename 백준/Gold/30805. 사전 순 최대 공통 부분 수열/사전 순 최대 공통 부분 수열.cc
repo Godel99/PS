468c4dba80633d5a229f401de6ce80e0b11ab60d
Godel99 <tylor9632@gmail.com>
2026-04-03 21:10:47 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.cc
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.cc
@@ -1,48 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using pii = pair<int, int>;
-
 
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    int n; cin >> n;
-    vector<pii> a(n);
-    for(int i = 0; i < n; i++){
-        int v; cin >> v;
-        a[i] = {v, i};
-    }
-    int m; cin >> m;
-    vector<pii> b(m);
-    for(int i = 0; i < m; i++){
-        int v; cin >> v;
-        b[i] = {v, i};
-    }
-    auto cmp = [](const pii& a, const pii& b) {
-        if(a.first != b.first) return a.first > b.first;
-        return a.second < b.second;
-    };
-    sort(a.begin(), a.end(), cmp);
-    sort(b.begin(), b.end(), cmp);
+    int n; cin >> n; vector<int> a(n); for(int i = 0; i < n; i++) cin >> a[i];
+    int m; cin >> m; vector<int> b(m); for(int i = 0; i < m; i++) cin >> b[i];
+    int maxl = 0;
+    for(int i = 0; i < n; i++) maxl = max(maxl, a[i]);
+    for(int i = 0; i < m; i++) maxl = max(maxl, b[i]);
     vector<int> ans;
-    int lia, lib; lia = lib = -1;
-    int pa, pb; pa = pb = 0;
-    while(pa < n && pb < m){
-        auto [va, ia] = a[pa];
-        auto [vb, ib] = b[pb];
-        if(va == vb){
-            if(ia > lia && ib > lib){
-                ans.push_back(va);
-                lia = ia, lib = ib;
-                pa++; pb++;
-            }
-            else{
-                if(ia <= lia) pa++;
-                else pb++;
-            }
+    auto acur = a.begin(), bcur = b.begin();
+    for(int x = maxl; x > 0; x--){
+        while(1){
+            auto anxt = find(acur, a.end(), x), bnxt = find(bcur, b.end(), x);
+            if(anxt == a.end() || bnxt == b.end()) break;
+            ans.push_back(x);
+            acur = next(anxt); bcur = next(bnxt);
         }
-        else if(va > vb) pa++;
-        else pb++;
     }
     cout << (int)ans.size() << '\n';
     for(int i = 0; i < ans.size(); i++) cout << ans[i] << ' ';
