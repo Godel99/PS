7915137ea6d0b24c2a8febb2eaeae537709638a5
Godel99 <tylor9632@gmail.com>
2026-04-04 21:15:40 +0900

    [Platinum IV] Title: I Am Knowledge, Time: 212 ms, Memory: 46592 KB -BaekjoonHub

---
+++ 백준/Platinum/28068. I Am Knowledge/I Am Knowledge.py
@@ -0,0 +1,24 @@
+import sys 
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+
+def main():
+    n = int(input())
+    pb, mb = [], []
+    for _ in range(n):
+        a, b = map(int, input().split())
+        if a <= b: pb.append((a, b))
+        else: mb.append((b, a))
+    pb.sort()
+    mb.sort(reverse=True)
+    joy = 0
+    for a, b in pb:
+        if a > joy: print(0); return
+        joy += b-a
+    for b, a in mb:
+        if a > joy: print(0); return
+        joy -= a-b
+    print(1)
+    return  
+if __name__ == '__main__':
+    main()
