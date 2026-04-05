c3a720f115ca0f13eb01b9fec2a95ed78abdd039
Godel99 <tylor9632@gmail.com>
2026-04-05 14:10:19 +0900

    [Gold III] Title: 치즈, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -6,44 +6,29 @@ from collections import deque
 
 def main():
     n, m = map(int, input().split())
-    dx, dy = (1, -1, 0, 0), (0, 0, 1, -1)
-    board = []
-    cheese = []
-    for i in range(n):
-        row = list(map(int, input().split()))
-        for j in range(m):
-            if row[j]: cheese.append((i, j))
-        board.append(row)
-    dq = deque([(0, 0)])
+    board = [list(map(int, input().split())) for _ in range(n)]
+    delta = ((1, 0), (-1, 0), (0, 1), (0, -1))
     board[0][0] = -1
-    while dq:
-        x, y = dq.popleft()
-        for dir in range(4):
-            nx, ny = x+dx[dir], y+dy[dir]
-            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 0:
-                board[nx][ny] = -1
-                dq.append((nx, ny))
-    time = 0
-    while cheese:
+    che = deque([(0, 0)])
+    time = -1
+    while che:
+        nche = deque()
+        while che:
+            r, c = che.popleft()
+            for dr, dc in delta:
+                nr, nc = r+dr, c+dc
+                if 0 <= nr < n and 0 <= nc < m:
+                    if board[nr][nc] < 0: continue
+                    if board[nr][nc] == 0:
+                        che.append((nr, nc))
+                        board[nr][nc] = -1
+                    elif board[nr][nc] == 1: board[nr][nc] = 2
+                    else:
+                        board[nr][nc] = -1
+                        nche.append((nr, nc))
+        che = nche
         time += 1
-        keep_cheese = []
-        melt_cheese = deque()
-        for x, y in cheese:
-            cnt = 0
-            for dir in range(4):
-                if board[x+dx[dir]][y+dy[dir]] == -1: cnt += 1
-            if cnt > 1: melt_cheese.append((x, y))
-            else: keep_cheese.append((x, y))
-        for x, y in melt_cheese: board[x][y] = -1
-        while melt_cheese:
-            x, y = melt_cheese.popleft()
-            for dir in range(4):
-                nx, ny = x+dx[dir], y+dy[dir]
-                if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 0:
-                    board[nx][ny] = -1
-                    melt_cheese.append((nx, ny))
-        cheese = keep_cheese
     print(time)
-    return  
+    return
 if __name__ == '__main__':
     main()
