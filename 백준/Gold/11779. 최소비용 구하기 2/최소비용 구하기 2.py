ce897b9fc8f11e77a64a354cbc02f2de436a69c5
Godel99 <tylor9632@gmail.com>
2026-04-05 23:35:52 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 260 ms, Memory: 60360 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,39 @@
+import sys 
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+
+from math import inf
+import heapq
+
+def main():
+    n = int(input())
+    m = int(input())
+    dp = [inf]*(n+1)
+    e = [[] for _ in range(n+1)]
+    par = [0]*(n+1)
+    for _ in range(m):
+        u, v, w = map(int, input().split())
+        e[u].append((v, w))
+    start, end = map(int, input().split())
+    dp[start] = 0
+    hq = []
+    heapq.heappush(hq, (0, start))
+    while hq:
+        cost, cur = heapq.heappop(hq)
+        if dp[cur] < cost: continue
+        for nxt, ncost in e[cur]:
+            if dp[nxt] > cost+ncost: 
+                dp[nxt] = cost+ncost
+                par[nxt] = cur
+                heapq.heappush(hq, (dp[nxt], nxt))
+    print(dp[end])
+    cur = end
+    ans = []
+    while cur:
+        ans.append(cur)
+        cur = par[cur]
+    print(len(ans))
+    ans.reverse()
+    print(*ans)
+if __name__ == '__main__':
+    main()
