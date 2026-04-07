02ffa1c58aee0debf014a9bc8e3ccc7cf209b63d
Godel99 <tylor9632@gmail.com>
2026-04-07 18:24:10 +0900

    [Platinum III] Title: Focusing on Costs, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Platinum/26868. Focusing on Costs/Focusing on Costs.cc
+++ 백준/Platinum/26868. Focusing on Costs/Focusing on Costs.cc
@@ -2,26 +2,28 @@
 using namespace std;
 using ll = long long;
 
+stack<string> ans;
+
+void gcd(int a, int b){
+    if(!a) return;
+    if(a == b) return ans.push("cos");
+    if(a > b){
+        ans.push("tan");
+        ans.push("acos");
+        ans.push("sin");
+        ans.push("atan");
+        return gcd(b, a);
+    }
+    ans.push("sin");
+    ans.push("atan");
+    return gcd(a, b-a);
+}
+
 int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
     int a, b; cin >> a >> b;
     a *= a; b *= b;
-    stack<string> ans;
-    while(a != b){
-        if(a < b){
-            b -= a;
-            ans.push("sin");
-            ans.push("atan");
-        }
-        else{
-            swap(a, b);
-            ans.push("tan");
-            ans.push("acos");
-            ans.push("sin");
-            ans.push("atan");
-        }
-    }
-    ans.push("cos");
+    gcd(a, b);
     cout << ans.size() << '\n';
     while(ans.size()){
         cout << ans.top() << ' '; ans.pop();
