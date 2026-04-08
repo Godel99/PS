c8ac53db1a4d605a3fbd30a7aec9b898e805aa9e
Godel99 <tylor9632@gmail.com>
2026-04-08 10:19:08 +0900

    [Gold V] Title: 트리와 쿼리, Time: 344 ms, Memory: 70936 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -7,19 +7,18 @@ sys.setrecursionlimit(10**7)
 def main():
     n, r, q = map(int, input().split())
     e = [[] for _ in range(n+1)]
-    vis = [0]*(n+1)
     subtsize = [0]*(n+1)
     for _ in range(n-1):
         u, v = map(int, input().split())
         e[u].append(v)
         e[v].append(u)
-    def dfs(cur):
-        vis[cur] = subtsize[cur] = 1
+    def dfs(cur, pre):
+        subtsize[cur] = 1
         for nxt in e[cur]:
-            if not vis[nxt]:
-                dfs(nxt)
-                subtsize[cur] += subtsize[nxt]                        
-    dfs(r)
+            if nxt == pre: continue
+            dfs(nxt, cur)
+            subtsize[cur] += subtsize[nxt]                        
+    dfs(r, 0)
     ans = []
     for _ in range(q):
         qry = int(input())
