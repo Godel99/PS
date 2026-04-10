45259acfa02879744009396c870f088abfa2113c
Godel99 <tylor9632@gmail.com>
2026-04-10 14:36:33 +0900

    [Gold IV] Title: 스도쿠, Time: 36 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -2,29 +2,87 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 
+FULL = 0b1111111110
+
 def main():
     board = [list(map(int, input())) for _ in range(9)]
-    blank = [(r, c) for r in range(9) for c in range(9) if board[r][c] == 0]
-    rowc = [[0]*10 for _ in range(9)]
-    colc = [[0]*10 for _ in range(9)]
-    boxc = [[0]*10 for _ in range(9)]
+    rowc, colc, boxc = [0]*9, [0]*9, [0]*9
+    def update(r, c, n, s):
+        bit = 1<<n
+        if s:
+            board[r][c] = n
+            rowc[r] |= bit
+            colc[c] |= bit
+            boxc[(r//3)*3+(c//3)] |= bit
+        else:
+            board[r][c] = 0
+            rowc[r] ^= bit
+            colc[c] ^= bit
+            boxc[(r//3)*3+(c//3)] ^= bit
     for r in range(9):
         for c in range(9):
             if board[r][c]:
-                num = board[r][c]
-                rowc[r][num] = colc[c][num] = boxc[(r//3)*3+(c//3)][num] = 1
+                bit = 1<<board[r][c]
+                rowc[r] |= bit
+                colc[c] |= bit
+                boxc[(r//3)*3+(c//3)] |= bit
+    def avail(r, c): return ~(rowc[r] | colc[c] | boxc[(r//3)*3+(c//3)])&FULL
+    def propa():
+        while 1:
+            flag = 0
+            for r in range(9):
+                for c in range(9):
+                    if board[r][c] == 0:
+                        ava = avail(r, c)
+                        if ava and (ava & (ava-1)) == 0:
+                            update(r, c, ava.bit_length()-1, 1)
+            for r in range(9):
+                arow = FULL&~rowc[r]
+                while arow:
+                    bit = arow&-arow
+                    num = bit.bit_length()-1
+                    nc, cnt = -1, 0
+                    for c in range(9):
+                        if board[r][c] == 0 and not (colc[c] & bit or boxc[(r//3)*3+(c//3)] & bit):
+                            cnt += 1
+                            nc = c
+                            if cnt > 1: break
+                    if cnt == 1:
+                        update(r, nc, num, 1)
+                        flag = 1
+                    arow &= ~bit
+            for c in range(9):
+                acol = FULL&~colc[c]
+                while acol:
+                    bit = acol&-acol
+                    num = bit.bit_length()-1
+                    nr, cnt = -1, 0
+                    for r in range(9):
+                        if board[r][c] == 0 and not (rowc[r] & bit or boxc[(r//3)*3+(c//3)] & bit):
+                            cnt += 1
+                            nr = r
+                            if cnt > 1: break
+                    if cnt == 1:
+                        update(nr, c, num, 1)
+                        flag = 1
+                    acol &= ~bit
+            if not flag: break
+    propa()
+    blank = [(r, c) for r in range(9) for c in range(9) if board[r][c] == 0]
     def dfs(idx):
         if idx == len(blank):
-            for row in board: print(''.join(map(str, row)))
-            exit(0)
+            for row in board:
+                print("".join(map(str, row)))
+            sys.exit(0)
         r, c = blank[idx]
-        for num in range(1, 10):
-            if not rowc[r][num] and not colc[c][num] and not boxc[(r//3)*3+(c//3)][num]:
-                rowc[r][num] = colc[c][num] = boxc[(r//3)*3+(c//3)][num] = 1
-                board[r][c] = num
-                dfs(idx+1)
-                rowc[r][num] = colc[c][num] = boxc[(r//3)*3+(c//3)][num] = 0
-                board[r][c] = 0
+        ava = avail(r, c)
+        while ava:
+            bit = ava & -ava
+            num = bit.bit_length() - 1
+            update(r, c, num, True)
+            dfs(idx + 1)
+            update(r, c, num, False)
+            ava &= ~bit
     dfs(0)
     return
 if __name__ == "__main__":
