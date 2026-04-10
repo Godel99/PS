236f4d1e069b5246a8b0f8a1b33fe9918af5c2bd
Godel99 <tylor9632@gmail.com>
2026-04-10 11:37:48 +0900

    [Platinum III] Title: Java2016, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/13482. Java2016/Java2016.cc
+++ 백준/Platinum/13482. Java2016/Java2016.cc
@@ -8,19 +8,16 @@ int main() {
     cout << "a=?max?\n";
     for(char c = 'b'; c <= 'p'; c++) cout << c << "=" << (char)(c-1) << "max" << (char)(c-1) << '\n';
     cout << "q=pmaxpmaxpmaxpmaxp\n";
-    cout << "r=q/q\n";
-    cout << "s=q-q\n";
-    for(char c = 't'; c <= 'z'; c++){
-        char pre = (c == 't') ? 'r' : (char)(c-1);
-        cout << c << '=' << pre << '+' << pre << '\n';
-    }
+    cout << "r=q-q\n";
+    cout << "s=q/q\n";
+    for(char c = 't'; c <= 'z'; c++) cout << c << '=' << (char)(c-1) << '+' << (char)(c-1) << '\n';
     if(n){
         vector<char> ans;
-        char bit[] = {'r', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char bit[] = {'s', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         for(int i = 0; i < 8; i++) if(n & (1<<i)) ans.push_back(bit[i]);
         cout << ans[0];
         for(int i = 1; i < ans.size(); i++) cout << '+' << ans[i];
     }
-    else cout << 's';
+    else cout << 'r';
     return 0;
 }
