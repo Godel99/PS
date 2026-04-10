49106242814f73d3f2971ab0faccdf86f7420f03
Godel99 <tylor9632@gmail.com>
2026-04-10 15:39:00 +0900

    [Platinum III] Title: Java2016, Time: 112 ms, Memory: 37036 KB -BaekjoonHub

--- 백준/Platinum/13482. Java2016/Java2016.py
+++ 백준/Platinum/13482. Java2016/Java2016.py
@@ -8,24 +8,18 @@ def main():
     n = int(input())
     a_z = ascii_lowercase
     print('a=?max?')
-    for i in range(1, 16):
-        cur, pre = a_z[i], a_z[i-1]
-        print(f'{cur}={pre}max{pre}')
+    for i in range(1, 16): print(f'{a_z[i]}={a_z[i-1]}max{a_z[i-1]}')
     print('q=pmaxpmaxpmaxpmaxp')
-    print('r=q/q')
-    print('s=q-q')
-    pre = 'r'
-    bit = ['r']
-    for i in range(19, 26):
-        print(f'{a_z[i]}={pre}+{pre}')
-        bit.append(a_z[i])
-        pre = a_z[i]
+    print('r=q-q')
+    print('s=q/q')
+    for i in range(19, 26): print(f'{a_z[i]}={a_z[i-1]}+{a_z[i-1]}')
     if n:
         ans = []
+        bit = ['s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         for i in range(8):
             if n & (1<<i): ans.append(bit[i])
         print('+'.join(ans))
-    else: print('s')
+    else: print('r')
     return
 if __name__ == "__main__":
     main()
