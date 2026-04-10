ec0108dac2de2a9c9653efe98caef475fbb7ef2f
Godel99 <tylor9632@gmail.com>
2026-04-10 15:23:08 +0900

    [Gold IV] Title: 스도쿠, Time: 1172 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -0,0 +1,107 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+
+FULL = 0b1111111110
+
+def main():
+    board = [list(map(int, input().split())) for _ in range(9)]
+    rowc, colc, boxc = [0]*9, [0]*9, [0]*9
+    def update(r, c, n, s):
+        bit = 1<<n
+        if s:
+            board[r][c] = n
+            rowc[r] |= bit
+            colc[c] |= bit
+            boxc[(r//3)*3+(c//3)] |= bit
+        else:
+            board[r][c] = 0
+            rowc[r] ^= bit
+            colc[c] ^= bit
+            boxc[(r//3)*3+(c//3)] ^= bit
+    for r in range(9):
+        for c in range(9):
+            if board[r][c]:
+                bit = 1<<board[r][c]
+                rowc[r] |= bit
+                colc[c] |= bit
+                boxc[(r//3)*3+(c//3)] |= bit
+    def avail(r, c): return ~(rowc[r] | colc[c] | boxc[(r//3)*3+(c//3)])&FULL
+    def propa():
+        while 1:
+            flag = 0
+            for r in range(9):
+                for c in range(9):
+                    if board[r][c] == 0:
+                        ava = avail(r, c)
+                        if ava and (ava & (ava-1)) == 0:
+                            update(r, c, ava.bit_length()-1, 1)
+            for r in range(9):
+                arow = FULL&~rowc[r]
+                while arow:
+                    bit = arow&-arow
+                    num = bit.bit_length()-1
+                    nc, cnt = -1, 0
+                    for c in range(9):
+                        if board[r][c] == 0 and not (colc[c] & bit or boxc[(r//3)*3+(c//3)] & bit):
+                            cnt += 1
+                            nc = c
+                            if cnt > 1: break
+                    if cnt == 1:
+                        update(r, nc, num, 1)
+                        flag = 1
+                    arow &= ~bit
+            for c in range(9):
+                acol = FULL&~colc[c]
+                while acol:
+                    bit = acol&-acol
+                    num = bit.bit_length()-1
+                    nr, cnt = -1, 0
+                    for r in range(9):
+                        if board[r][c] == 0 and not (rowc[r] & bit or boxc[(r//3)*3+(c//3)] & bit):
+                            cnt += 1
+                            nr = r
+                            if cnt > 1: break
+                    if cnt == 1:
+                        update(nr, c, num, 1)
+                        flag = 1
+                    acol &= ~bit
+            for b in range(9):
+                abox = FULL&~boxc[b]
+                while abox:
+                    bit = abox&-abox
+                    num = bit.bit_length()-1
+                    nr, nc, cnt = -1, -1, 0
+                    for i in range(9):
+                        r = (b//3)*3+(i//3)
+                        c = (b%3)*3+(i%3)
+                        if board[r][c] == 0:
+                            if not (rowc[r]&bit or colc[c]&bit):
+                                cnt += 1
+                                nr, nc = r, c
+                                if cnt > 1: break
+                    if cnt == 1:
+                        update(nr, nc, num, 1)
+                        flag = 1
+                    abox &= ~bit
+            if not flag: break
+    propa()
+    blank = [(r, c) for r in range(9) for c in range(9) if board[r][c] == 0]
+    def dfs(idx):
+        if idx == len(blank):
+            for row in board:
+                print(" ".join(map(str, row)))
+            sys.exit(0)
+        r, c = blank[idx]
+        ava = avail(r, c)
+        while ava:
+            bit = ava & -ava
+            num = bit.bit_length() - 1
+            update(r, c, num, 1)
+            dfs(idx + 1)
+            update(r, c, num, 0)
+            ava &= ~bit
+    dfs(0)
+    return
+if __name__ == "__main__":
+    main()
