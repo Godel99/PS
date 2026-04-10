1ada020101ea34b30c1a23ec97597513ddab0f21
Godel99 <tylor9632@gmail.com>
2026-04-10 10:07:04 +0900

    [Platinum III] Title: Java2016, Time: 112 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Platinum/13482. Java2016/Java2016.py
+++ 백준/Platinum/13482. Java2016/Java2016.py
@@ -1,54 +1,31 @@
 import sys
+input = lambda: sys.stdin.readline().rstrip()
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 
-def solve():
-    input_data = sys.stdin.read().split()
-    if not input_data:
-        return
-    
-    c = int(input_data[0])
+from string import ascii_lowercase
 
-    # 1. 확률 증폭 및 2의 거듭제곱 매크로 정의
-    macros = [
-        "a=? max ?",
-        "b=a max a",
-        "c_m=b max b", # 'c'는 입력값 변수와 겹치지 않게 주의 (출력은 'c'로 유지)
-        "d=c_m max c_m".replace("c_m", "c"),
-        "e=d max d",
-        "f=e max e",
-        "g=f max f",
-        "h=g max g",
-        "i=h max h",
-        "j=i max i",
-        "k=j max j",
-        "l=k max k",
-        "m=l max l",  # m은 99.9999% 확률로 255
-        "n=m / m",    # n = 1
-        "o=n + n",    # o = 2
-        "p=o + o",    # p = 4
-        "q=p + p",    # q = 8
-        "r=q + q",    # r = 16
-        "s=r + r",    # s = 32
-        "t=s + s",    # t = 64
-        "u=t + t"     # u = 128
-    ]
-
-    # 매크로 문자열 내 임시 변수 처리 복구
-    macros[2] = "c=b max b"
-    macros[3] = "d=c max c"
-
-    result = "\n".join(macros) + "\n"
-
-    # 2. 목표 상수 조립
-    if c == 0:
-        result += "n - n\n"
-    else:
-        terms = []
+def main():
+    n = int(input())
+    a_z = ascii_lowercase
+    print('a=?max?')
+    for i in range(1, 16):
+        cur, pre = a_z[i], a_z[i-1]
+        print(f'{cur}={pre}max{pre}')
+    print('q=pmaxpmaxpmaxpmaxp')
+    print('r=q/q')
+    print('s=q-q')
+    pre = 'r'
+    bit = ['r']
+    for i in range(19, 26):
+        print(f'{a_z[i]}={pre}+{pre}')
+        bit.append(a_z[i])
+        pre = a_z[i]
+    if n:
+        ans = []
         for i in range(8):
-            if (c >> i) & 1:
-                terms.append(chr(ord('n') + i))
-        result += " + ".join(terms) + "\n"
-
-    print(result)
-
-if __name__ == '__main__':
-    solve()
+            if n & (1<<i): ans.append(bit[i])
+        print('+'.join(ans))
+    else: print('s')
+    return
+if __name__ == "__main__":
+    main()
