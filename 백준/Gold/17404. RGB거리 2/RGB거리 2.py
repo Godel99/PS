c6b7d2a1c73ead43098dcc8aac126651ec9cff58
Godel99 <tylor9632@gmail.com>
2026-04-12 11:42:16 +0900

    [Gold IV] Title: RGB거리 2, Time: 36 ms, Memory: 35560 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -7,7 +7,7 @@ from math import inf
 def main():
     n = int(input())
     cost = [[*map(int, input().split())] for _ in range(n)]
-    ans = []
+    ans = inf
     for c in range(3):
         dp = [[inf]*3 for _ in range(n)]
         dp[0][c] = cost[0][c]
@@ -15,8 +15,9 @@ def main():
             dp[i][0] = min(dp[i-1][1], dp[i-1][2])+cost[i][0]
             dp[i][1] = min(dp[i-1][0], dp[i-1][2])+cost[i][1]
             dp[i][2] = min(dp[i-1][0], dp[i-1][1])+cost[i][2]
-        ans.append(min(dp[-1][(c+1)%3], dp[-1][(c+2)%3]))
-    print(min(ans))
+        for i in range(3):
+            if i != c: ans = min(ans, dp[-1][i])
+    print(ans)
     return
 if __name__ == "__main__":
     main()
