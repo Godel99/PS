549abf73997d18c7587424d6713f72686e7fd285
Godel99 <tylor9632@gmail.com>
2026-04-14 22:09:29 +0900

    [Gold III] Title: 줄 세우기, Time: 160 ms, Memory: 41692 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,28 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+
+from collections import deque
+
+def main():
+    n, m = map(int, input().split())
+    e = [[] for _ in range(n+1)]
+    deg = [0]*(n+1)
+    for _ in range(m):
+        u, v = map(int, input().split())
+        e[u].append(v)
+        deg[v] += 1
+    dq = deque()
+    for i in range(1, n+1):
+        if deg[i] == 0: dq.append(i)
+    ans = []
+    while dq:
+        cur = dq.popleft()
+        ans.append(cur)
+        for nxt in e[cur]:
+            deg[nxt] -= 1
+            if deg[nxt] == 0: dq.append(nxt)
+    print(' '.join(map(str, ans)))
+    return
+if __name__ == "__main__":
+    main()
