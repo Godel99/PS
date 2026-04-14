0c0fbcde9ef1ff8b22a0a8107074a580d531606e
Godel99 <tylor9632@gmail.com>
2026-04-14 17:52:27 +0900

    [Platinum III] Title: Minimum Spanning Arborescence, Time: 136 ms, Memory: 112288 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/33807. Minimum Spanning Arborescence/Minimum Spanning Arborescence.py
+++ 백준/Platinum/33807. Minimum Spanning Arborescence/Minimum Spanning Arborescence.py
@@ -14,7 +14,6 @@ def main():
     for i in range(2, n+1):
         if deg[i]: vcnt[deg[i]] += 1
         if deg[i] > maxd: maxd = deg[i]
-    fact = [1]*(maxd+3)
     invf = [1]*(maxd+3)
     fact = 1
     for i in range(1, maxd+3): fact = (fact * i)%MOD
