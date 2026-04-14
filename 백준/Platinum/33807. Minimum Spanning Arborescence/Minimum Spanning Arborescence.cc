36cf71009ec993a67b24cb7400367e34a4edd85f
Godel99 <tylor9632@gmail.com>
2026-04-15 08:45:02 +0900

    [Platinum III] Title: Minimum Spanning Arborescence, Time: 8 ms, Memory: 2416 KB, Score: 16 point -BaekjoonHub

--- 백준/Platinum/33807. Minimum Spanning Arborescence/Minimum Spanning Arborescence.cc
+++ 백준/Platinum/33807. Minimum Spanning Arborescence/Minimum Spanning Arborescence.cc
@@ -4,7 +4,8 @@ using ll = long long;
 
 const ll MOD = 998244353;
 int k;
-vector<ll> invf;
+vector<ll> invf, pref, suff, invki;
+
 
 ll pw(ll n, ll r, ll MOD){
     n %= MOD;
@@ -21,16 +22,9 @@ ll Sd(int d){
     vector<ll> y(d+2, 0);
     for(int i = 1; i < d+2; i++) y[i] = (y[i-1]+pw(i, d, MOD))%MOD;
     if(k <= d+1) return y[k];
-    vector<ll> pref(d+2, 1), suff(d+2, 1);
-    pref[0] = k;
-    for(int i = 1; i < d+2; i++) pref[i] = (pref[i-1]*(k-i))%MOD;
-    suff[d+1] = k-d-1;
-    for(int i = d; i > 0; i--) suff[i] = (suff[i+1]*(k-i))%MOD;
     ll ret = 0;
     for(int i = 0; i < d+2; i++){
-        ll num = 1;
-        if(i) num = (num*pref[i-1])%MOD;
-        if(i < d+1) num = (num*suff[i+1])%MOD;
+        ll num = (pref[d+1]*invki[i])%MOD;
         ll den = (invf[i]*invf[d+1-i])%MOD;
         if((d+1-i)%2) den = MOD-den;
         ret = (ret+y[i]*num%MOD*den)%MOD;
@@ -52,15 +46,26 @@ int main() {
         if(deg[i]) vcnt[deg[i]]++;
         if(deg[i] > maxd) maxd = deg[i];
     }
+    int limit = min(k-1, maxd+1);
+    pref.assign(limit+1, 0), suff.assign(limit+1, 0);
+    pref[0] = k;
+    for(int i = 1; i <= limit; i++) pref[i] = (pref[i-1]*(k-i))%MOD;
+    suff[limit] = pw(pref[limit], MOD-2, MOD);
+    for(int i = limit-1; i > 0; i--) suff[i] = (suff[i+1]*(k-i-i))%MOD;
+    invki.assign(limit+1, 0);
+    invki[0] = suff[0];
+    for(int i = 1; i <= limit; i++) invki[i] = (pref[i-1]*suff[i])%MOD;
     ll fact = 1; invf.assign(maxd+3, 1); 
     for(int i = 1; i < maxd+3; i++) fact = (fact*i)%MOD;
     invf[maxd+2] = pw(fact, MOD-2, MOD);
     for(int i = maxd+1; i > 0; i--) invf[i] = (invf[i+1]*(i+1))%MOD;
     ll ans = 0;
     ll invk = pw(k, MOD-2, MOD);
+    vector<ll> invkp(maxd+1, 1);
+    for(int i = 1; i <= maxd; i++) invkp[i] = (invkp[i-1]*invk)%MOD;
     for(int d = 1; d <= maxd; d++){
         if(vcnt[d] == 0) continue;
-        ll evmd = (Sd(d)*pw(invk, d, MOD))%MOD;
+        ll evmd = (Sd(d)*invkp[d])%MOD;
         ans = (ans+vcnt[d]*evmd)%MOD;
     }
     cout << ans;
