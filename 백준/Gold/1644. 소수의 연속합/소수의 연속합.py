bdbf262b1cdb2a1b3c6f890e0537199be0878dc9
Godel99 <tylor9632@gmail.com>
2026-04-15 13:58:19 +0900

    [Gold III] Title: 소수의 연속합, Time: 172 ms, Memory: 58012 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -7,15 +7,15 @@ def main():
     if n == 1: print(0); return
     if n < 5: print(1 if n in {2, 3} else 0); return
     size = n//3+1
-    isp = bytearray([1])*size
+    isp = [1]*size
     isp[0] = 0
     limit = int(n**0.5)
     for i in range(1, limit//3+1):
         if isp[i]:
             p = 3*i+1|1
             d, s, j = p*2, p*p, p*(p+4-2*(i&1))
-            isp[s//3::d] = bytearray([0])*((size-1-s//3)//d+1)
-            isp[j//3::d] = bytearray([0])*((size-1-j//3)//d+1)
+            isp[s//3::d] = [0]*((size-1-s//3)//d+1)
+            isp[j//3::d] = [0]*((size-1-j//3)//d+1)
     primes = [2, 3]+[3*i+1|1 for i in range(1, size) if isp[i]]
     ans = sp = l = 0
     for rp in primes:
