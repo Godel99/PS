a30dda87fb0eb1359cf298cb8002cc61fcab8e66
Godel99 <tylor9632@gmail.com>
2026-04-16 10:34:12 +0900

    [Platinum IV] Title: 전구, Time: 104 ms, Memory: 111000 KB -BaekjoonHub

--- 백준/Platinum/2449. 전구/전구.py
+++ 백준/Platinum/2449. 전구/전구.py
@@ -9,16 +9,15 @@ def main():
     for i in range(1, n):
         if b[i] != b[i-1]: a.append(b[i])
     m = len(a)
-    dp = [[-1]*(m) for _ in range(m)]
-    def df(l, r):
-        if l >= r: return 0
-        if dp[l][r] != -1: return dp[l][r]
-        ret = float('inf')
-        for i in range(l, r):
-            ret = min(ret, df(l, i)+df(i+1, r)+(a[l] != a[i+1]))
-        dp[l][r] = ret
-        return ret
-    print(df(0, m-1))
+    dp = [[0]*(m) for _ in range(m)]
+    for d in range(1, m):
+        for l in range(m-d):
+            r = l+d
+            mind = d
+            for mid in range(l, r):
+                mind = min(mind, dp[l][mid]+dp[mid+1][r]+(a[l] != a[mid+1]))
+            dp[l][r] = mind
+    print(dp[0][-1])
     return
 if __name__ == "__main__":
     main()
