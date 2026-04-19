2bed2ecf4fbc92b20b7af821ac85b20eed9259f4
Godel99 <tylor9632@gmail.com>
2026-04-19 14:18:33 +0900

    [Platinum III] Title: 트리의 MEX, Time: 468 ms, Memory: 223248 KB -BaekjoonHub

--- 백준/Platinum/25549. 트리의 MEX/트리의 MEX.py
+++ 백준/Platinum/25549. 트리의 MEX/트리의 MEX.py
@@ -2,31 +2,35 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 
-sys.setrecursionlimit(200000)
-
 def main():
     n = int(input())
     p = [0]+[*map(int, input().split())]
     e = [[] for _  in range(n+1)]
     s = [set() for _ in range(n+1)]
     v = [0]+[*map(int, input().split())]
-    root = -1
+    deg = [0]*(n+1)
     for i in range(1, n+1):
-        if p[i] == -1: root = i
-        else:e[p[i]].append(i)
+        if p[i] == -1: continue
+        deg[p[i]] += 1
+        e[p[i]].append(i)
+    st = [i for i in range(1, n+1) if deg[i] == 0]
     ans = [0]*n
-    def dfs(cur):
+    while st:
+        cur = st.pop()
         s[cur].add(v[cur])
         prv = 0
-        for nxt in e[cur]:
-            dfs(nxt)
-            prv = max(prv, ans[nxt-1])
-            if len(s[nxt]) > len(s[cur]): s[cur], s[nxt] = s[nxt], s[cur]
-            s[cur].update(s[nxt])
-            s[nxt].clear()
-        while prv in s[cur]: prv += 1
-        ans[cur-1] = prv
-    dfs(root)
+        for nxt in e[cur]: prv = max(prv, ans[nxt-1])
+        for nv in range(prv, n+1):
+            if nv not in s[cur]:
+                ans[cur-1] = nv
+                break
+        if p[cur] == -1: continue
+        if len(s[p[cur]]) >= len(s[cur]): s[p[cur]].update(s[cur])
+        else:
+            s[cur].update(s[p[cur]])
+            s[p[cur]] = s[cur]
+        deg[p[cur]] -= 1
+        if deg[p[cur]] == 0: st.append(p[cur])
     print('\n'.join(map(str, ans)))
     return
 if __name__ == "__main__":
