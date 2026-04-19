852aaa68298b4801a8c53067477edc1fd99f9ae9
Godel99 <tylor9632@gmail.com>
2026-04-19 12:05:49 +0900

    [Platinum III] Title: 트리의 MEX, Time: 1200 ms, Memory: 678856 KB -BaekjoonHub

--- 백준/Platinum/25549. 트리의 MEX/트리의 MEX.py
+++ 백준/Platinum/25549. 트리의 MEX/트리의 MEX.py
@@ -2,36 +2,29 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 
+sys.setrecursionlimit(200000)
+
 def main():
     n = int(input())
     p = [0]+[*map(int, input().split())]
     e = [[] for _  in range(n+1)]
-    s = [set() for _ in range(n+1)]
     v = [0]+[*map(int, input().split())]
-    deg = [0]*(n+1)
     for i in range(1, n+1):
-        if p[i] == -1:continue
-        deg[p[i]] += 1
-        e[p[i]].append(i)
-    st = [i for i in range(1, n+1) if deg[i] == 0]
+        if p[i] == -1: root = i
+        else: e[p[i]].append(i)
     ans = [0]*n
-    while st:
-        cur = st.pop()
-        s[cur].add(v[cur])
-        prv = 0
-        for nxt in e[cur]: prv = max(prv, ans[nxt-1])
-        for nv in range(prv, n+1):
-            if nv not in s[cur]:
-                ans[cur-1] = nv
-                break
-        if p[cur] == -1: continue
-        if len(s[p[cur]]) >= len(s[cur]):
-            for nv in s[cur]: s[p[cur]].add(nv)
-        else:
-            for nv in s[p[cur]]: s[cur].add(nv)
-            s[p[cur]] = s[cur]
-        deg[p[cur]] -= 1
-        if deg[p[cur]] == 0: st.append(p[cur])
+    def dfs(cur):
+        cs = {v[cur]}
+        cmex = 0
+        for nxt in e[cur]:
+            nc, nmex = dfs(nxt)
+            if len(nc) > len(cs): cs, nc = nc, cs
+            cs.update(nc)
+            cmex = max(cmex, nmex)
+        while cmex in cs: cmex += 1
+        ans[cur-1] = cmex
+        return cs, cmex
+    dfs(root)
     print('\n'.join(map(str, ans)))
     return
 if __name__ == "__main__":
