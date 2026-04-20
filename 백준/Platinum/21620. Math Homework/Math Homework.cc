ce577d925f1943e730305fd384f4ce242bbd8fb5
Godel99 <tylor9632@gmail.com>
2026-04-20 17:38:29 +0900

    [Platinum III] Title: Math Homework, Time: 84 ms, Memory: 28972 KB -BaekjoonHub

--- 백준/Platinum/21620. Math Homework/Math Homework.cc
+++ 백준/Platinum/21620. Math Homework/Math Homework.cc
@@ -1,40 +1,12 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-using ai3 = array<int, 3>;
-
-int main(){
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    int n, m; cin >> n >> m;
-    vector<ai3> qry;
-    vector<vector<int>> d(17, vector<int>(n+2, 0));
-    while(m--){
-        int x, y, z; cin >> x >> y >> z;
-        qry.push_back({x, y, z});
-        d[z][x]++;
-        d[z][y+1]--;
-    }
-    vector<int> ans(n, 1);
-    for(int z = 1; z <= 16; z++){
-        int cnt = 0;
-        for(int i = 1; i <= n; i++){
-            cnt += d[z][i];
-            if(cnt) ans[i-1] = lcm(ans[i-1], z);
-        }
-    }
-    vector<int> tree(2*n, 0);
-    for(int i = 0; i < n; i++) tree[n+i] = ans[i];
-    for(int i = n-1; i > 0; i--) tree[i] = gcd(tree[i<<1], tree[i<<1|1]);
-    for(auto [x, y, z] : qry){
-        int res = 0, l = x+n-1, r = y+n-1;
-        while(l <= r){
-            if(l&1) res = gcd(res, tree[l++]);
-            if(~r&1) res = gcd(res, tree[r--]);
-            l >>= 1;
-            r >>= 1;
-        }
-        if(res != z) return !(cout << "Impossible");
-    }
-    for(int i : ans) cout << i << ' ';
-    return 0;
-}
+<html>
+<head><title>504 Gateway Time-out</title><script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=yCEQmMLHY64IevK_GvBNkNJTyNLTewf6dY2_1-_unrfM71TEvLvjudX0p3ULar8sTegpM1--nVz-jZL-iAAE_97Oqx-BcTfTB5qU6RMc12E" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly93d3cuYWNtaWNwYy5uZXQvc291cmNlL2Rvd25sb2FkLzEwNTEwNTI4NQ"/></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
