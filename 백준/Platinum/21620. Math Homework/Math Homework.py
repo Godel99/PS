bf88cf2ab44944939ef6a19a01b0befb6014430c
Godel99 <tylor9632@gmail.com>
2026-04-20 17:32:20 +0900

    [Platinum III] Title: Math Homework, Time: 264 ms, Memory: 186392 KB -BaekjoonHub

--- 백준/Platinum/21620. Math Homework/Math Homework.py
+++ 백준/Platinum/21620. Math Homework/Math Homework.py
@@ -1,38 +1,75 @@
 import sys
-input = lambda: sys.stdin.readline().rstrip()
-print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
-
-from math import gcd, lcm
+from math import gcd
 
 def main():
-    n, m = map(int, input().split())
-    qry = []
-    d = [[0]*(n+2) for _ in range(17)]
-    for _ in range(m):
-        x, y, z =  map(int, input().split())
-        qry.append((x, y, z))
-        d[z][x] += 1
-        d[z][y+1] -= 1
-    ans = [1]*n
-    for z in range(1, 17):
-        cnt = 0
-        for i in range(1, n+1):
-            cnt += d[z][i]
-            if cnt:
-                ans[i-1] = lcm(ans[i-1], z)
-    tree = [0]*2*n
-    for i in range(n): tree[n+i] = ans[i]
-    for i in range(n-1, 0, -1): tree[i] = gcd(tree[i<<1], tree[i<<1|1])
-    for x, y, z in qry:
-        res = 0
-        l, r = x+n-1, y+n-1
-        while l <= r:
-            if l&1: res = gcd(res, tree[l]); l += 1
-            if ~r&1: res = gcd(res, tree[r]); r -= 1
-            l >>= 1
-            r >>= 1
-        if res != z: print('Impossible'); return
-    print(*ans)
-    return
-if __name__ == "__main__":
-    main()
+    data = sys.stdin.buffer.read().split()
+    ptr = 0
+    N = int(data[ptr]); ptr += 1
+    M = int(data[ptr]); ptr += 1
+
+    xs = [int(data[ptr + 3*i]) - 1 for i in range(M)]
+    ys = [int(data[ptr + 3*i + 1]) - 1 for i in range(M)]
+    zs = [int(data[ptr + 3*i + 2]) for i in range(M)]
+
+    D = [1] * N
+
+    # 각 소수별로 각 위치에서 필요한 최대 지수를 차이 배열로 계산
+    for p in [2, 3, 5, 7, 11, 13]:
+        # p^max_e <= 16인 최대 지수
+        max_e, pk = 0, p
+        while pk <= 16:
+            max_e += 1
+            pk *= p
+
+        # 각 제약조건의 z에서 p의 지수 계산
+        vp = [0] * M
+        for i in range(M):
+            t = zs[i]
+            while t % p == 0:
+                vp[i] += 1
+                t //= p
+
+        max_exp = [0] * N
+
+        # 지수 e별로 차이 배열(difference array)로 범위 업데이트
+        for e in range(1, max_e + 1):
+            diff = [0] * (N + 1)
+            for i in range(M):
+                if vp[i] >= e:
+                    diff[xs[i]] += 1
+                    diff[ys[i] + 1] -= 1
+            cur = 0
+            for i in range(N):
+                cur += diff[i]
+                if cur > 0:
+                    max_exp[i] = e  # 낮은 e→높은 e 순서로 덮어쓰면 max와 동일
+
+        pw = [p**e for e in range(max_e + 1)]
+        for i in range(N):
+            if max_exp[i]:
+                D[i] *= pw[max_exp[i]]
+
+    # Sparse Table 구성 (구간 GCD O(1) 질의)
+    LOG = N.bit_length()
+    table = [D[:]]
+    for j in range(LOG):
+        half = 1 << j
+        prev = table[j]
+        table.append([
+            gcd(prev[i], prev[i + half]) if i + half < N else prev[i]
+            for i in range(N)
+        ])
+
+    def range_gcd(l, r):
+        k = (r - l + 1).bit_length() - 1
+        return gcd(table[k][l], table[k][r - (1 << k) + 1])
+
+    # 모든 조건 검증
+    for i in range(M):
+        if range_gcd(xs[i], ys[i]) != zs[i]:
+            sys.stdout.write("Impossible\n")
+            return
+
+    sys.stdout.write(' '.join(map(str, D)) + '\n')
+
+main()
