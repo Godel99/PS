d28cb7d3dee5f9a34e46e2b711d5c0a6f5e1fd59
Godel99 <tylor9632@gmail.com>
2026-04-20 18:01:45 +0900

    [Platinum III] Title: Math Homework, Time: 272 ms, Memory: 191808 KB -BaekjoonHub

--- 백준/Platinum/21620. Math Homework/Math Homework.py
+++ 백준/Platinum/21620. Math Homework/Math Homework.py
@@ -1,75 +1,75 @@
 import sys
 from math import gcd
 
-def main():
-    data = sys.stdin.buffer.read().split()
-    ptr = 0
-    N = int(data[ptr]); ptr += 1
-    M = int(data[ptr]); ptr += 1
+def solve():
+    # 1. 입력 속도 최적화: 전체 데이터를 한 번에 읽어와 쪼갭니다.
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+    
+    N = int(input_data[0])
+    M = int(input_data[1])
+    
+    ptr = 2
+    queries = []
+    # z별 차분 배열 (1~16)
+    diff = [[0] * (N + 2) for _ in range(17)]
+    
+    for _ in range(M):
+        x = int(input_data[ptr])
+        y = int(input_data[ptr+1])
+        z = int(input_data[ptr+2])
+        ptr += 3
+        queries.append((x, y, z))
+        # 차분 배열에 마킹
+        diff[z][x] += 1
+        diff[z][y + 1] -= 1
 
-    xs = [int(data[ptr + 3*i]) - 1 for i in range(M)]
-    ys = [int(data[ptr + 3*i + 1]) - 1 for i in range(M)]
-    zs = [int(data[ptr + 3*i + 2]) for i in range(M)]
+    # 2. D[i] (ans) 계산: 1부터 16까지 루프 돌며 LCM 적용
+    ans = [1] * N
+    for z in range(1, 17):
+        cnt = 0
+        dz = diff[z]
+        for i in range(1, N + 1):
+            cnt += dz[i]
+            if cnt > 0:
+                # ans[i-1] = lcm(ans[i-1], z)
+                curr = ans[i-1]
+                ans[i-1] = (curr * z) // gcd(curr, z)
 
-    D = [1] * N
+    # 3. Sparse Table 구성 (O(N log N))
+    # Python에서 2차원 리스트 접근은 st[j][i] 형태가 빠릅니다.
+    st = [ans]
+    
+    # N의 길이에 따른 최대 로그 값 계산
+    max_log = N.bit_length()
+    for j in range(1, max_log):
+        prev = st[j-1]
+        offset = 1 << (j - 1)
+        # 현재 레벨에서 계산 가능한 구간까지만 gcd 계산
+        curr = [0] * (N - (1 << j) + 1)
+        for i in range(N - (1 << j) + 1):
+            curr[i] = gcd(prev[i], prev[i + offset])
+        st.append(curr)
 
-    # 각 소수별로 각 위치에서 필요한 최대 지수를 차이 배열로 계산
-    for p in [2, 3, 5, 7, 11, 13]:
-        # p^max_e <= 16인 최대 지수
-        max_e, pk = 0, p
-        while pk <= 16:
-            max_e += 1
-            pk *= p
-
-        # 각 제약조건의 z에서 p의 지수 계산
-        vp = [0] * M
-        for i in range(M):
-            t = zs[i]
-            while t % p == 0:
-                vp[i] += 1
-                t //= p
-
-        max_exp = [0] * N
-
-        # 지수 e별로 차이 배열(difference array)로 범위 업데이트
-        for e in range(1, max_e + 1):
-            diff = [0] * (N + 1)
-            for i in range(M):
-                if vp[i] >= e:
-                    diff[xs[i]] += 1
-                    diff[ys[i] + 1] -= 1
-            cur = 0
-            for i in range(N):
-                cur += diff[i]
-                if cur > 0:
-                    max_exp[i] = e  # 낮은 e→높은 e 순서로 덮어쓰면 max와 동일
-
-        pw = [p**e for e in range(max_e + 1)]
-        for i in range(N):
-            if max_exp[i]:
-                D[i] *= pw[max_exp[i]]
-
-    # Sparse Table 구성 (구간 GCD O(1) 질의)
-    LOG = N.bit_length()
-    table = [D[:]]
-    for j in range(LOG):
-        half = 1 << j
-        prev = table[j]
-        table.append([
-            gcd(prev[i], prev[i + half]) if i + half < N else prev[i]
-            for i in range(N)
-        ])
-
-    def range_gcd(l, r):
-        k = (r - l + 1).bit_length() - 1
-        return gcd(table[k][l], table[k][r - (1 << k) + 1])
-
-    # 모든 조건 검증
-    for i in range(M):
-        if range_gcd(xs[i], ys[i]) != zs[i]:
+    # 4. 검증 (O(1) 쿼리)
+    results = []
+    for l, r, z in queries:
+        # 0-based 인덱스로 변환
+        l_idx, r_idx = l - 1, r - 1
+        length = r_idx - l_idx + 1
+        # k = floor(log2(length))
+        k = length.bit_length() - 1
+        
+        # Sparse Table을 이용한 O(1) GCD 조회
+        res = gcd(st[k][l_idx], st[k][r_idx - (1 << k) + 1])
+        
+        if res != z:
             sys.stdout.write("Impossible\n")
             return
 
-    sys.stdout.write(' '.join(map(str, D)) + '\n')
+    # 5. 결과 출력 최적화
+    sys.stdout.write(" ".join(map(str, ans)) + "\n")
 
-main()
+if __name__ == "__main__":
+    solve()
