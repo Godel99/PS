b3118156fee3623f8438b5a60e7368884ca7abab
Godel99 <tylor9632@gmail.com>
2026-04-21 22:17:20 +0900

    [level 4] Title: 도둑질, Time: 8.58 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/README.md
+++ 프로그래머스/4/42897. 도둑질/README.md
@@ -1,10 +1,10 @@
 # [level 4] 도둑질 - 42897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897?language=cpp) 
 
 ### 성능 요약
 
-메모리: 90.2 MB, 시간: 690.16 ms
+메모리: 40.5 MB, 시간: 8.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 21일 22:12:25
+2026년 04월 21일 22:17:17
 
 ### 문제 설명
 
---
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int solution(vector<int> money) {
+    int n = money.size();
+    vector<int> dp1(n, 0), dp2(n, 0);
+    dp1[0] = money[0], dp1[1] = max(money[0], money[1]);
+    dp2[0] = 0, dp2[1] = money[1];
+    for(int i = 2; i < n-1; i++) dp1[i] = max(dp1[i-1], dp1[i-2]+money[i]);
+    for(int i = 2; i < n; i++) dp2[i] = max(dp2[i-1], dp2[i-2]+money[i]);
+    return max(dp1[n-2], dp2[n-1]);
+}
