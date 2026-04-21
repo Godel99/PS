93ea607aaf4781b4eace6b6986d388bac3c0c602
Godel99 <tylor9632@gmail.com>
2026-04-21 15:14:10 +0900

    [Platinum III] Title: 흑백조경사, Time: 168 ms, Memory: 32876 KB -BaekjoonHub

--- 백준/Platinum/32869. 흑백조경사/흑백조경사.cc
+++ 백준/Platinum/32869. 흑백조경사/흑백조경사.cc
@@ -1,103 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n;
-    cin >> n;
-
-    vector<int> c(n + 1);
-    int W = 0, B = 0;
-    for (int i = 1; i <= n; i++) {
+int main(){
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vector<vector<int>> e(n+1);
+    vector<int> c(n+1, 0), isEven(n+1, 0), w(n+1, 0), b(n+1, 0), cnt(n+1, 0), ans;
+    int tw = 0, tb = 0;
+    for(int i = 1; i <= n; i++){
         cin >> c[i];
-        if (c[i] == 0) W++; else B++;
+        if(c[i]) tb++;
+        else tw++;
     }
-    int D = W - B;
-    auto val = [&](int i) -> int { return c[i] == 0 ? 1 : -1; };
-
-    vector<vector<int>> adj(n + 1);
-    vector<int> deg(n + 1, 0);
-    for (int i = 0; i < n - 1; i++) {
+    for(int i = 0; i < n-1; i++){
         int u, v; cin >> u >> v;
-        adj[u].push_back(v); adj[v].push_back(u);
-        deg[u]++; deg[v]++;
+        e[u].push_back(v);
+        e[v].push_back(u);
     }
-
-    // 1번 기준 DFS: in/out time, 서브트리 합 f[] 계산
-    vector<int> in_t(n+1), out_t(n+1), par(n+1,0), f(n+1,0);
-    vector<vector<int>> ch(n+1);
-    int timer = 0;
-    {
-        vector<bool> vis(n+1, false);
-        vector<int> it(n+1, 0), stk;
-        stk.reserve(n); stk.push_back(1); vis[1] = true;
-        while (!stk.empty()) {
-            int v = stk.back();
-            if (it[v] == 0) { in_t[v] = ++timer; f[v] = val(v); }
-            bool pushed = false;
-            while (it[v] < (int)adj[v].size()) {
-                int u = adj[v][it[v]++];
-                if (!vis[u]) {
-                    vis[u] = true; par[u] = v;
-                    ch[v].push_back(u); stk.push_back(u);
-                    pushed = true; break;
-                }
-            }
-            if (!pushed) {
-                out_t[v] = timer;
-                if (par[v]) f[par[v]] += f[v];
-                stk.pop_back();
-            }
+    function<void(int, int)> dfs = [&](int cur, int pre){
+        isEven[cur] = 1;
+        if(c[cur]) b[cur] = 1;
+        else w[cur] = 1;
+        for(int nxt : e[cur]){
+            if(pre == nxt) continue;
+            dfs(nxt, cur);
+            b[cur] += b[nxt];
+            w[cur] += w[nxt];
+            isEven[cur] &= isEven[nxt];
+            if(!isEven[nxt]) cnt[cur]++;
         }
-    }
-
-    // 차이 배열로 나쁜 루트 마킹
-    vector<int> diff(n + 2, 0);
-    auto mark = [&](int l, int r) {
-        if (l > r) return;
-        diff[l]++; diff[r+1]--;
+        if(c[cur] && (w[cur] >= b[cur]) || !c[cur] && (b[cur] >= w[cur])) isEven[cur] = 0;
     };
-
-    for (int v = 1; v <= n; v++) {
-        int sv = val(v);
-
-        // 루트 조건: r = v
-        if ((D - sv) * sv < 0)
-            mark(in_t[v], in_t[v]);
-
-        if (deg[v] >= 2) {
-            // 정적 조건: r ∉ subtree(v)
-            int sd = f[v] - sv;
-            if (sd * sv < 0) {
-                mark(1, in_t[v] - 1);
-                mark(out_t[v] + 1, n);
-            }
-            // 자식 조건: r ∈ subtree(u)
-            for (int u : ch[v]) {
-                int dn = D - f[u] - sv;
-                if (dn * sv < 0)
-                    mark(in_t[u], out_t[u]);
-            }
+    function<void(int, int)> sol = [&](int cur, int pre){
+        if(!cnt[cur] && (c[cur] && (tw < tb) || !c[cur] && (tb < tw))) ans.push_back(cur);
+        if(cnt[cur] >= 2) return;
+        for(int nxt : e[cur]){
+            if(nxt == pre) continue;
+            if(cnt[cur] && isEven[nxt]) continue;
+            if(c[cur] && (tw-w[nxt] >= tb-b[nxt]) || !c[cur] && (tb-b[nxt] >= tw-w[nxt])) continue;
+            sol(nxt, cur);
         }
-    }
-
-    // 전위합 계산
-    for (int i = 1; i <= n; i++) diff[i] += diff[i-1];
-
-    // 유효 루트 수집
-    vector<int> ans;
-    for (int v = 1; v <= n; v++)
-        if (diff[in_t[v]] == 0)
-            ans.push_back(v);
-
-    cout << ans.size() << "\n";
-    for (int i = 0; i < (int)ans.size(); i++) {
-        if (i) cout << " ";
-        cout << ans[i];
-    }
-    if (!ans.empty()) cout << "\n";
-
+    };
+    dfs(1, 1);
+    sol(1, 1);
+    sort(ans.begin(), ans.end());
+    cout << ans.size() << '\n';
+    for(auto i : ans) cout << i << ' ';
     return 0;
 }
