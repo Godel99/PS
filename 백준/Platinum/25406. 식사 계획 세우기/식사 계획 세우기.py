8d858326e801317d6af1820f1035729885690ced
Godel99 <tylor9632@gmail.com>
2026-04-22 20:42:35 +0900

    [Platinum III] Title: 식사 계획 세우기, Time: 260 ms, Memory: 165772 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/25406. 식사 계획 세우기/식사 계획 세우기.py
+++ 백준/Platinum/25406. 식사 계획 세우기/식사 계획 세우기.py
@@ -1,89 +1,63 @@
 import sys
-from collections import Counter, defaultdict
+input = lambda: sys.stdin.readline().rstrip()
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 
 def main():
-    data = sys.stdin.buffer.read().split()
-    n = int(data[0])
-    arr = list(map(int, data[1:n+1]))
-
-    cnt = Counter(arr)
-    buckets = defaultdict(set)  # count -> {value, ...}
-    for v, c in cnt.items():
-        buckets[c].add(v)
-
-    max_cnt = max(cnt.values())
-
-    if 2 * max_cnt - 1 > n:
-        sys.stdout.write("-1\n")
-        return
-
-    def remove(v):
-        nonlocal max_cnt
-        c = cnt[v]
-        buckets[c].discard(v)
-        cnt[v] = c - 1
-        if c - 1 > 0:
-            buckets[c - 1].add(v)
-        if not buckets[max_cnt]:
-            max_cnt -= 1
-
-    res = [0] * n
+    n = int(input())
+    a = [*map(int, input().split())]
+    ans = [0]*n
+    cnt, head, nxt, pre = [0]*(n+1), [-1]*(n+1), [-1]*(n+1), [-1]*(n+1)
     vis = bytearray(n)
-    i = 0
-    unvis_size = n
+    mcnt = 0
+    for i in range(n): cnt[a[i]] += 1
+    def Add(c, x):
+        nxt[x] = head[c]
+        if head[c] != -1: pre[head[c]] = x
+        head[c] = x
+    def Remove(c, x):
+        nonlocal mcnt
+        if pre[x] != -1: nxt[pre[x]] = nxt[x]
+        else: head[c] = nxt[x]
+        if nxt[x] != -1: pre[nxt[x]] = pre[x]
+        pre[x] = nxt[x] = -1
+        cnt[x] -= 1
+        Add(cnt[x], x)
+        if head[mcnt] == -1: mcnt -= 1
+    for i in range(1, n+1):
+        if cnt[i]:
+            Add(cnt[i], i)
+            if mcnt < cnt[i]: mcnt = cnt[i]
+    if 2*mcnt-1 > n: print(-1); return
+    idx, size = 0, n
+    p1, p2 = 0, 1
+    while p2 < n and a[p2-1] == a[p2]: p2 += 1   # 경계 체크
+    while idx < n and 2*mcnt-1 < size:
+        while p1 < n and (vis[p1] or (p2 < n and a[p1] == a[p2])): p1 += 1
+        if p1 >= n: break
+        vis[p1] = 1; size -= 1
+        Remove(cnt[a[p1]], a[p1])
+        ans[idx] = p1+1; idx += 1
+        if idx == n or 2*mcnt-1 == size: break     # and → or
+        while p2 < n and (vis[p2] or a[p2] == a[p1]): p2 += 1
+        if p2 >= n: break
+        vis[p2] = 1; size -= 1
+        Remove(cnt[a[p2]], a[p2])
+        ans[idx] = p2+1; idx += 1
+    maxv = head[mcnt]
+    p1 = 0
+    while p1 < n and vis[p1]: p1 += 1
+    p2 = p1
+    while idx < n:
+        while p1 < n and (vis[p1] or a[p1] != maxv): p1 += 1
+        if p1 >= n: break
+        vis[p1] = 1
+        ans[idx] = p1+1; idx += 1
+        if idx == n: break
+        while p2 < n and (vis[p2] or a[p2] == maxv): p2 += 1
+        if p2 >= n: break
+        vis[p2] = 1
+        ans[idx] = p2+1; idx += 1
+    print(*ans)
 
-    # ── Phase 1 ────────────────────────────────────────────────────
-    if 2 * max_cnt - 1 < unvis_size:
-        pos1 = 0
-        pos2 = 1
-        while pos2 < n and arr[pos2 - 1] == arr[pos2]:
-            pos2 += 1
-
-        while i < n and 2 * max_cnt - 1 < unvis_size:
-            while pos1 < n and (vis[pos1] or (pos2 < n and arr[pos1] == arr[pos2])):
-                pos1 += 1
-            if pos1 >= n:
-                break
-            vis[pos1] = 1; unvis_size -= 1
-            remove(arr[pos1])
-            res[i] = pos1; i += 1
-
-            if i == n or 2 * max_cnt - 1 == unvis_size:
-                break
-
-            while pos2 < n and (vis[pos2] or arr[pos2] == arr[pos1]):
-                pos2 += 1
-            if pos2 >= n:
-                break
-            vis[pos2] = 1; unvis_size -= 1
-            remove(arr[pos2])
-            res[i] = pos2; i += 1
-
-    # ── Phase 2 ────────────────────────────────────────────────────
-    max_val = next(iter(buckets[max_cnt]))  # head[max_cnt] 대체
-    pos1 = 0
-    while pos1 < n and vis[pos1]:
-        pos1 += 1
-    pos2 = pos1
-
-    while i < n:
-        while pos1 < n and (vis[pos1] or arr[pos1] != max_val):
-            pos1 += 1
-        if pos1 >= n:
-            break
-        vis[pos1] = 1
-        res[i] = pos1; i += 1
-
-        if i == n:
-            break
-
-        while pos2 < n and (vis[pos2] or arr[pos2] == max_val):
-            pos2 += 1
-        if pos2 >= n:
-            break
-        vis[pos2] = 1
-        res[i] = pos2; i += 1
-
-    sys.stdout.write(' '.join(map(str, (r + 1 for r in res))) + '\n')
-
-main()
+if __name__ == "__main__":
+    main()
