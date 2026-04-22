720e8e94befa1f11340efd8fad1fc83facc05388
Godel99 <tylor9632@gmail.com>
2026-04-22 20:51:01 +0900

    [Platinum III] Title: 식사 계획 세우기, Time: 252 ms, Memory: 165716 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/25406. 식사 계획 세우기/식사 계획 세우기.py
+++ 백준/Platinum/25406. 식사 계획 세우기/식사 계획 세우기.py
@@ -5,8 +5,7 @@ print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args
 def main():
     n = int(input())
     a = [*map(int, input().split())]
-    ans = [0]*n
-    cnt, head, nxt, pre = [0]*(n+1), [-1]*(n+1), [-1]*(n+1), [-1]*(n+1)
+    ans, cnt, head, nxt, pre = [0]*n, [0]*(n+1), [-1]*(n+1), [-1]*(n+1), [-1]*(n+1)
     vis = bytearray(n)
     mcnt = 0
     for i in range(n): cnt[a[i]] += 1
@@ -18,7 +17,7 @@ def main():
         nonlocal mcnt
         if pre[x] != -1: nxt[pre[x]] = nxt[x]
         else: head[c] = nxt[x]
-        if nxt[x] != -1: pre[nxt[x]] = pre[x]
+        if nxt[x] != -1: pre[nxt[x]] = pre[x]    
         pre[x] = nxt[x] = -1
         cnt[x] -= 1
         Add(cnt[x], x)
@@ -30,34 +29,32 @@ def main():
     if 2*mcnt-1 > n: print(-1); return
     idx, size = 0, n
     p1, p2 = 0, 1
-    while p2 < n and a[p2-1] == a[p2]: p2 += 1   # 경계 체크
+    while p2 < n and a[p2-1] == a[p2]: p2 += 1
     while idx < n and 2*mcnt-1 < size:
-        while p1 < n and (vis[p1] or (p2 < n and a[p1] == a[p2])): p1 += 1
-        if p1 >= n: break
-        vis[p1] = 1; size -= 1
+        while p1 < n and (a[p1] == a[p2] or vis[p1]): p1 += 1
+        vis[p1] = 1
+        size -= 1
         Remove(cnt[a[p1]], a[p1])
         ans[idx] = p1+1; idx += 1
-        if idx == n or 2*mcnt-1 == size: break     # and → or
-        while p2 < n and (vis[p2] or a[p2] == a[p1]): p2 += 1
-        if p2 >= n: break
-        vis[p2] = 1; size -= 1
+        if idx == n or 2*mcnt-1 == size: break
+        while p2 < n and (a[p1] == a[p2] or vis[p2]): p2 += 1
+        vis[p2] = 1
+        size -= 1
         Remove(cnt[a[p2]], a[p2])
         ans[idx] = p2+1; idx += 1
     maxv = head[mcnt]
     p1 = 0
-    while p1 < n and vis[p1]: p1 += 1
+    while vis[p1]: p1 += 1
     p2 = p1
-    while idx < n:
-        while p1 < n and (vis[p1] or a[p1] != maxv): p1 += 1
-        if p1 >= n: break
+    while(idx < n):
+        while p1 < n and (a[p1] != maxv or vis[p1]): p1 += 1
         vis[p1] = 1
         ans[idx] = p1+1; idx += 1
         if idx == n: break
-        while p2 < n and (vis[p2] or a[p2] == maxv): p2 += 1
-        if p2 >= n: break
+        while p2 < n and (a[p2] == maxv or vis[p2]): p2 += 1
         vis[p2] = 1
         ans[idx] = p2+1; idx += 1
     print(*ans)
-
+    return
 if __name__ == "__main__":
     main()
