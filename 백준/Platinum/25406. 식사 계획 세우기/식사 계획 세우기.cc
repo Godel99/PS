defdc4cde8a8816245264bd11638fc18ab06b687
Godel99 <tylor9632@gmail.com>
2026-04-22 21:03:37 +0900

    [Platinum III] Title: 식사 계획 세우기, Time: 64 ms, Memory: 9212 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/25406. 식사 계획 세우기/식사 계획 세우기.cc
+++ 백준/Platinum/25406. 식사 계획 세우기/식사 계획 세우기.cc
@@ -1,129 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector< queue<int> > v;
-int plan[321098];
-int num[321098];
-int a[321098];
-int cntfromsize[321098];
-int cntfromnumber[321098];
-int n;
+using ll = long long;
 
-struct rest{
-	int idx, minnum, size;
-	friend bool operator<(const rest& a, const rest& b){
-		if(a.size == b.size){
-		  return a.minnum > b.minnum;
-		}
-		return a.size < b.size;
-	}
-};
-
-struct Rest{
-	int idx, minnum, size;
-	friend bool operator<(const Rest& a, const Rest& b){
-		if(a.minnum == b.minnum){
-			return a.size < b.size;
-		}
-		return a.minnum > b.minnum;
-	}
-};
-
-priority_queue<rest> pq_by_size;
-priority_queue<Rest> pq_by_number;
-
-int main() {
-	cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-	cin >> n;
-	v.resize(n+1);
-	int m = -1;
-	for(int i = 1; i <= n; i++){
-		cin >> a[i];
-		v[a[i]].push(i);
-		num[a[i]]++;
-		if(num[a[i]] > m) m = num[a[i]];
-	}
-	if(n%2){
-		if(m > n/2+1) return !(cout << -1);
-	}
-	else{
-		if(m > n/2) return !(cout << -1);
-	}
-	for(int i = 1; i <= n; i++){
-		if(num[i]){
-			rest r;
-			Rest R;
-			r.idx = R.idx = i;
-			r.minnum = R.minnum = v[i].front();
-			r.size = R.size = num[i];
-			pq_by_size.push(r);
-			pq_by_number.push(R);
-		}
-	}
-
-	int lastidx = 0;
-	rest remain;
-	Rest Remain;
-	bool re = false;
-	bool Re = false;
-	for(int i = n; i > 0; i--){
-		int t = pq_by_size.top().size;
-		if((t >= i/2+i%2 && i%2 == 1) || (t > i/2 && t%2 == 0)){
-			rest p = pq_by_size.top();
-			pq_by_size.pop();
-			if(cntfromnumber[p.idx]){
-				p.minnum = v[p.idx].front();
-				p.size -= cntfromnumber[p.idx];
-				cntfromnumber[p.idx] = 0;
-				if(p.size > 0) pq_by_size.push(p);
-				i++;
-				continue;
-			}
-			else if(lastidx == p.idx){
-				re = true;
-				remain = p;
-				i++;
-				continue;
-			}
-			else{
-				cout << p.minnum << ' ';
-				v[p.idx].pop();
-				p.minnum = v[p.idx].front();
-				p.size--;
-				lastidx = p.idx;
-				if(p.size > 0) pq_by_size.push(p);
-				cntfromsize[p.idx]++;
-			}
-		}
-
-		else{
-		   Rest p = pq_by_number.top();
-			pq_by_number.pop();
-			if(cntfromsize[p.idx]){
-				p.minnum = v[p.idx].front();
-				p.size -= cntfromsize[p.idx];
-				cntfromsize[p.idx] = 0;
-				if(p.size > 0) pq_by_number.push(p);
-				i++;
-				continue;
-			}
-			else if(lastidx == p.idx){
-				Re = true;
-				Remain = p;
-				i++;
-				continue;
-			}
-			else{
-				cout << p.minnum << ' ';
-				v[p.idx].pop();
-				p.minnum = v[p.idx].front();
-				p.size--;
-				lastidx = p.idx;
-				if(p.size > 0) pq_by_number.push(p);
-				cntfromnumber[p.idx]++;
-			}
-		}
-		if(re) pq_by_size.push(remain);
-		if(Re) pq_by_number.push(Remain);
-		re = Re = false;
-	}
+int main(){
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vector<int> a(n);
+    vector<int> ans(n, 0), cnt(n+1, 0), head(n+1, -1), nxt(n+1, -1), pre(n+1, -1);
+    vector<bool> vis(n, 0);
+    int mcnt = 0;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        cnt[a[i]]++;
+    }
+    function<void(int, int)> Add = [&](int c, int x){
+        nxt[x] = head[c];
+        if(head[c] != -1) pre[head[c]] = x;
+        head[c] = x;
+    };
+    function<void(int, int)> Remove = [&](int c, int x){
+        if(pre[x] != -1) nxt[pre[x]] = nxt[x];
+        else head[c] = nxt[x];
+        if(nxt[x] != -1) pre[nxt[x]] = pre[x];
+        pre[x] = nxt[x] = -1;
+        cnt[x]--;
+        Add(cnt[x], x);
+        if(head[mcnt] == -1) mcnt--;
+    };
+    for(int i = 1; i <= n; i++) if(cnt[i]){
+        Add(cnt[i], i);
+        if(mcnt < cnt[i]) mcnt = cnt[i];
+    }
+    if(2*mcnt-1 > n) return !(cout << -1);
+    int idx = 0, size = n, p1 = 0, p2 = 1;
+    while(p2 < n && a[p2-1] == a[p2]) p2++;
+    while(idx < n && 2*mcnt-1 < size){
+        while(a[p1] == a[p2] || vis[p1]) p1++;
+        vis[p1] = 1;
+        size--;
+        Remove(cnt[a[p1]], a[p1]);
+        ans[idx++] = p1+1;
+        if(idx == n || 2*mcnt-1 == size) break;
+        while(a[p1] == a[p2] or vis[p2]) p2++;
+        vis[p2] = 1;
+        size--;
+        Remove(cnt[a[p2]], a[p2]);
+        ans[idx++] = p2+1;
+    }
+    int maxv = head[mcnt];
+    p1 = 0;
+    while(vis[p1]) p1++;
+    p2 = p1;
+    while(idx < n){
+        while(a[p1] != maxv || vis[p1]) p1++;
+        vis[p1] = 1;
+        ans[idx++] = p1+1;
+        if(idx == n) break;
+        while(a[p2] == maxv || vis[p2]) p2++;
+        vis[p2] = 1;
+        ans[idx++] = p2+1;
+    }
+    for(int i = 0; i < n; i++) cout << ans[i] << ' ';
+    return 0;
 }
