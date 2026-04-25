45983681f7aedde34fb2b69d39c3f8429907d1e7
Godel99 <tylor9632@gmail.com>
2026-04-25 20:52:39 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 180 ms, Memory: 119664 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
@@ -6,18 +6,18 @@ def main():
     n, m = map(int, input().split())
     board = [input() for _ in range(n)]
     vis = [[0]*m for _ in range(n)]
-    ans = 0; idx = 1
+    ans = idx = 0
     move = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
     for i in range(n):
         for j in range(m):
             if vis[i][j]: continue
+            idx += 1
             x, y = i, j
             while not vis[x][y]:
                 vis[x][y] = idx
                 dx, dy = move[board[x][y]]
                 x, y = x+dx, y+dy
             if vis[x][y] == idx: ans += 1
-            idx += 1
     print(ans)
     return
 if __name__ == "__main__":
