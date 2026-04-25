68bd3c0c5659a27c94b20d40fcbb8261dcb78437
Godel99 <tylor9632@gmail.com>
2026-04-25 20:33:00 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 276 ms, Memory: 72000 KB -BaekjoonHub

---
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
@@ -0,0 +1,24 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+
+def main():
+    n, m = map(int, input().split())
+    board = [input() for _ in range(n)]
+    vis = [[0]*m for _ in range(n)]
+    ans = 0; idx = 1
+    move = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+    for i in range(n):
+        for j in range(m):
+            if vis[i][j]: continue
+            x, y = i, j
+            while not vis[x][y]:
+                vis[x][y] = idx
+                dx, dy = move[board[x][y]]
+                x, y = x+dx, y+dy
+            if vis[x][y] == idx: ans += 1
+            idx += 1
+    print(ans)
+    return
+if __name__ == "__main__":
+    main()
