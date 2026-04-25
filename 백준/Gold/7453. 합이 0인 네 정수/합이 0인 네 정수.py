6457c1a417d59e9539f1e86964c194b5bb13b56a
Godel99 <tylor9632@gmail.com>
2026-04-25 10:13:52 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 668 ms, Memory: 128324 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -3,34 +3,37 @@ input = lambda: sys.stdin.readline().rstrip()
 print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
 
 def main():
+    SIZE = 2**20
     n = int(input())
     A, B, C, D = [0]*n, [0]*n, [0]*n, [0]*n
-    for i in range(n): A[i], B[i], C[i], D[i] = map(int, input().split())
-    sAB, sCD = [0]*n*n, [0]*n*n
-    idx = 0
-    for i in range(n):
-        for j in range(n):
-            sAB[idx] = A[i]+B[j]
-            sCD[idx] = C[i]+D[j]
-            idx += 1
-    sAB.sort()
-    sCD.sort()
-    ans = 0
-    l, r = 0, len(sCD)-1
-    while l < len(sAB) and r >= 0:
-        vAB = sAB[l]
-        vCD = sCD[r]
-        if vAB+vCD > 0: r -= 1
-        elif vAB+vCD < 0: l += 1
-        else:
-            cntAB = cntCD = 0
-            while l < len(sAB) and sAB[l] == vAB:
-                cntAB += 1
-                l += 1
-            while r >= 0 and sCD[r] == vCD:
-                cntCD += 1
-                r -= 1
-            ans += cntAB*cntCD
+    for i in range(n): 
+        A[i], B[i], C[i], D[i] = map(int, input().split())
+        C[i] = -C[i]; D[i] = -D[i]
+    A.sort(); B.sort(); C.sort(); D.sort()
+    AB = CD = cnt = ans = 0 
+    LM = min(A[0]+B[0], C[0]+D[0])+SIZE-1
+    NB, ND = [0]*n, [0]*n
+    RK, RS = [0]*SIZE, [0]*SIZE
+    while AB < n and CD < n:
+        cnt += 1
+        for i in range(AB, n):
+            l = LM - A[i]
+            while NB[i] < n:
+                s = l-B[NB[i]]
+                if s < 0: break
+                if RK[s] == cnt: RS[s] += 1
+                else: RK[s] = cnt; RS[s] = 1
+                NB[i] += 1
+                if NB[i] == n: AB += 1
+        for i in range(CD, n):
+            l = LM-C[i]
+            while ND[i] < n:
+                s = l-D[ND[i]]
+                if s < 0: break
+                if RK[s] == cnt: ans += RS[s]
+                ND[i] += 1
+                if ND[i] == n: CD += 1
+        LM += SIZE
     print(ans)
     return
 if __name__ == "__main__":
