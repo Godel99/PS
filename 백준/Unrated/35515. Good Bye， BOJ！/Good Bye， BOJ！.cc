5c26d1ab67da54c9e4f0d2d6e3357d002c0de2cb
Godel99 <tylor9632@gmail.com>
2026-04-27 18:22:41 +0900

    [Unrated] Title: Good Bye, BOJ!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/35515. Good Bye， BOJ！/Good Bye， BOJ！.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    cout << "그 동안 정말 즐거웠어, 고마워 BOJ!";
+    return 0;
+}
