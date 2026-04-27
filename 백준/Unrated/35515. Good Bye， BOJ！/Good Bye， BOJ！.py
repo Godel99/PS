7dfc1fc1b1a5bf3b9a035a3099701f297d04bfdc
Godel99 <tylor9632@gmail.com>
2026-04-27 18:21:26 +0900

    [Unrated] Title: Good Bye, BOJ!, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Unrated/35515. Good Bye， BOJ！/Good Bye， BOJ！.py
@@ -0,0 +1,9 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+
+def main():
+    print('BOJ')
+    return
+if __name__ == "__main__":
+    main()
