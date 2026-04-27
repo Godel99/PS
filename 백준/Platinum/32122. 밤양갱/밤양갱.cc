878c2add191f6fc22cdfc0c611704adae19f23bd
Godel99 <tylor9632@gmail.com>
2026-04-27 15:42:26 +0900

    [Platinum III] Title: 밤양갱, Time: 320 ms, Memory: 3156 KB -BaekjoonHub

--- 백준/Platinum/32122. 밤양갱/밤양갱.cc
+++ 백준/Platinum/32122. 밤양갱/밤양갱.cc
@@ -1,106 +1,63 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using ai5 = array<int, 5>;
 
-// N=100일 때 최대 칸 수는 10,000개, 그룹당 최대 5,000개
-int n;
-int adj[5005][4]; // 인접 리스트를 고정 배열로 하여 캐시 효율 증대
-int adj_cnt[5005];
-int mA[5005], mB[5005];
-int vis[5005], timer;
-bool isActive[2][5005];
-
-struct Cell {
-    int v, r, c, type, id;
-    bool operator<(const Cell& o) const { return v < o.v; }
-};
-
-// DFS: 오직 Set 0의 정점에서만 시작함
-bool dfs(int u) {
-    vis[u] = timer;
-    for (int i = 0; i < adj_cnt[u]; ++i) {
-        int v = adj[u][i];
-        if (!isActive[1][v]) continue; // 상대방 칸이 아직 활성화되지 않음
-        
-        if (!mB[v] || (vis[mB[v]] != timer && dfs(mB[v]))) {
-            mA[u] = v;
-            mB[v] = u;
-            return true;
-        }
-    }
-    return false;
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    if (!(cin >> n)) return 0;
-
-    vector<Cell> cells;
+int main(){
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    const int dx[] = {1, -1, 0, 0}, dy[] = {0, 0, 1, -1};
+    int n, timer = 0, maxn; cin >> n; maxn = n*n/2+1;
+    vector<vector<int>> e(maxn), idx(n, vector<int>(n));
+    vector<int> vis(maxn, 0), mA(maxn, 0), mB(maxn, 0);
+    vector<ai5> cells;
+    vector<vector<bool>> isAct(2, vector<bool>(maxn, 0));
     int cnt[2] = {0, 0};
-    int id_map[105][105];
-    int type_map[105][105];
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            int val; cin >> val;
-            int type = (i + j) % 2;
-            id_map[i][j] = ++cnt[type];
-            type_map[i][j] = type;
-            cells.push_back({val, i, j, type, id_map[i][j]});
+    for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j){
+        int x; cin >> x;
+        int type = (i+j)%2;
+        idx[i][j] = ++cnt[type];
+        cells.push_back({x, i, j, type, idx[i][j]});
+    }
+    sort(cells.begin(), cells.end());
+    for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j){
+        if((i+j)%2) continue;
+        for(int dir = 0; dir < 4; dir++){
+            int ni = i+dx[dir], nj = j+dy[dir];
+            if (ni < 0 || ni >= n || nj < 0 || nj >= n) continue;
+            e[idx[i][j]].push_back(idx[ni][nj]);
         }
     }
-
-    sort(cells.begin(), cells.end());
-
-    // 간선 미리 계산 (Set 0에서 Set 1로 가는 단방향 간선만 저장)
-    int dr[] = {0, 1, 0, -1}, dc[] = {1, 0, -1, 0};
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if ((i + j) % 2 != 0) continue; // Set 0만 기준으로 간선 생성
-            int u = id_map[i][j];
-            for (int k = 0; k < 4; ++k) {
-                int ni = i + dr[k], nj = j + dc[k];
-                if (ni < 0 || ni >= n || nj < 0 || nj >= n) continue;
-                adj[u][adj_cnt[u]++] = id_map[ni][nj];
+    function<bool(int)> dfs = [&](int cur){
+        vis[cur] = timer;
+        if(!isAct[0][cur]) return false;
+        for(int nxt : e[cur]){
+            if(!isAct[1][nxt]) continue;
+            if(!mB[nxt] || (vis[mB[nxt]] != timer && dfs(mB[nxt]))){
+                mA[cur] = nxt;
+                mB[nxt] = cur;
+                return true;
             }
         }
-    }
-
-    vector<int> unmatched_A; // 아직 매칭되지 않은 Set 0 정점들
-    int total_match = 0;
-    int limit = n * n / 2;
-
-    for (int i = 0; i < n * n; ++i) {
-        int type = cells[i].type;
-        int id = cells[i].id;
-        isActive[type][id] = true;
-
-        if (type == 0) unmatched_A.push_back(id);
-
-        // 새로운 칸이 추가되었을 때 매칭이 늘어날 가능성이 있는지 확인
-        // 매칭 시도는 항상 '매칭되지 않은' Set 0 정점들에서만 수행
+        return false;
+    };
+    vector<int> unmatch;
+    int total = 0, limit = n*n/2;
+    for(int i = 0; i < n*n; i++){
+        int x = cells[i][0];
+        int type = cells[i][3];
+        int id = cells[i][4];
+        isAct[type][id] = 1;
+        if(!type) unmatch.push_back(id);
         timer++;
-        bool found = false;
-        
-        // 최적화: 새로 추가된 칸이 매칭을 발생시킬 수 있는 후보들만 조사
-        for (int j = 0; j < (int)unmatched_A.size(); ++j) {
-            int u = unmatched_A[j];
-            if (dfs(u)) {
-                // 매칭 성공 시 리스트에서 제거 (swap & pop_back)
-                unmatched_A[j] = unmatched_A.back();
-                unmatched_A.pop_back();
-                found = true;
-                total_match++;
-                cout << cells[i].v << "\n";
+        for(int j = 0; j < unmatch.size(); j++){
+            if(dfs(unmatch[j])){
+                unmatch[j] = unmatch.back(); unmatch.pop_back();
+                cout << x << '\n';
+                total++;
                 break;
             }
         }
-        if (total_match == limit) break;
+        if(total == limit) break;
     }
-
     return 0;
 }
