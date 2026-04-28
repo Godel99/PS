e95bb1792651cfd3d5d8231382b6c7884c0b9260
Godel99 <tylor9632@gmail.com>
2026-04-28 10:49:12 +0900

    [Gold II] Title: 1의 개수 세기, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
@@ -0,0 +1,18 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+
+def main():
+    a, b = map(int, input().split())
+    memo = {0: 0, 1: 1}
+    def bitcnt(n):
+        if n in memo: return memo[n]
+        k = 0
+        while 1 << (k+1) <= n: k += 1
+        msb = 1<<k
+        memo[n] = bitcnt(msb-1)+(n-msb+1)+bitcnt(n-msb)
+        return memo[n]
+    print(bitcnt(b) - bitcnt(a-1))
+    return
+if __name__ == "__main__":
+    main()
